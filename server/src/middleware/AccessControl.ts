84e97ac0c2e44f30d9774e3c70500e702a659001
Dudrie <Dudrie@gmx.de>
2019-08-16 15:24:38 +0200

    Add role based access control function.

--- server/src/middleware/AccessControl.ts
+++ server/src/middleware/AccessControl.ts
@@ -1,4 +1,5 @@
 import { NextFunction, Request, Response } from 'express';
+import { Role } from 'shared/dist/model/Role';
 
 /**
  * Checks if the User of the request is authenticated. If he is NOT than the request is immediatly aborted and a status of 401 is returned.
@@ -13,4 +14,45 @@ export function isAuthenticated(req: Request, res: Response, next: NextFunction)
   } else {
     res.status(401).send();
   }
+}
+
+/**
+ * Checks if the user is logged in and if one of his/her roles match the one(s) passed to this function. If any of the premises is FALSE then a 401 is responded and the request is therefore canceled.
+ *
+ * Note: The user object in the request has to contain a `roles` array. Otherwise a 401 is responded aswell.
+ *
+ * @param roles Array of all roles (or single role) which have access to the endpoint.
+ */
+export function checkRoleAccess(roles: Role | Role[]) {
+  if (!Array.isArray(roles)) {
+    roles = [roles];
+  }
+
+  return [
+    isAuthenticated,
+
+    (req: Request, res: Response, next: NextFunction) => {
+      if (!req.user) {
+        return res.status(401).send();
+      }
+
+      if (!('roles' in req.user)) {
+        return res.status(401).send();
+      }
+
+      const userRoles: unknown = req.user['roles'];
+
+      if (!Array.isArray(userRoles)) {
+        return res.status(401).send();
+      }
+
+      for (const role of roles) {
+        if (userRoles.includes(role)) {
+          return next();
+        }
+      }
+
+      return res.status(401).send();
+    },
+  ];
 }
--- server/src/routes/user.ts
+++ server/src/routes/user.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
-import { isAuthenticated } from '../middleware/AccessControl';
+import { isAuthenticated, checkRoleAccess } from '../middleware/AccessControl';
 import userService from '../services/UserService';
 import { User } from 'shared/dist/model/User';
+import { Role } from 'shared/dist/model/Role';
 
 const userRouter = Router();
 
-userRouter.get('/', isAuthenticated, async (req, res) => {
+userRouter.get('/', ...checkRoleAccess(Role.ADMIN), async (_, res) => {
   const allUsers: User[] = await userService.getAllUsers();
 
   res.json(allUsers);
