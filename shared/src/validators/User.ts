f53a5a2f848a4b3026c0f7aad90a83d2f71dea24
Dudrie <Dudrie@gmx.de>
2019-08-25 10:34:04 +0200

    Make roles and tutorials arrays required in UserDTO.

--- shared/src/validators/User.ts
+++ shared/src/validators/User.ts
@@ -7,8 +7,10 @@ import { validateSchema } from './helper';
 const UserDTOSchema = Yup.object().shape<UserDTO>({
   firstname: Yup.string().required(),
   lastname: Yup.string().required(),
-  roles: Yup.array<Role>(),
-  tutorials: Yup.array().of(Yup.string()),
+  roles: Yup.array<Role>().required(),
+  tutorials: Yup.array()
+    .required()
+    .of(Yup.string()),
 });
 
 const CreateUserDTOSchema = Yup.object().shape<CreateUserDTO>({
@@ -16,8 +18,10 @@ const CreateUserDTOSchema = Yup.object().shape<CreateUserDTO>({
   lastname: Yup.string().required(),
   username: Yup.string().required(),
   password: Yup.string().required(),
-  roles: Yup.array<Role>(),
-  tutorials: Yup.array().of(Yup.string()),
+  roles: Yup.array<Role>().required(),
+  tutorials: Yup.array()
+    .required()
+    .of(Yup.string()),
 });
 
 export function validateAgainstCreateUserDTO(
