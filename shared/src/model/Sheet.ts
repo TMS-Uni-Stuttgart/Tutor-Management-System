7586d8725d79a058984cecc4cb6b774a0524a34c
Dudrie <Dudrie@gmx.de>
2019-08-28 14:58:40 +0200

    Add Sheet models to shared package.

---
+++ shared/src/model/Sheet.ts
@@ -0,0 +1,26 @@
+import { HasId } from './Common';
+
+export interface Sheet extends HasId {
+  bonusSheet: boolean;
+  exercises: Exercise[];
+  id: string;
+  sheetNo: number;
+}
+
+export interface Exercise {
+  bonus: boolean;
+  exNo: number;
+  maxPoints: number;
+}
+
+export interface SheetDTO {
+  bonusSheet: boolean;
+  exercises: ExerciseDTO[];
+  sheetNo: number;
+}
+
+export interface ExerciseDTO {
+  bonus: boolean;
+  exNo: number;
+  maxPoints: number;
+}
