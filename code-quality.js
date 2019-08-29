8b8f764fd9ae4764959200b26673896d2a8e84d2
Dudrie <Dudrie@gmx.de>
2019-08-29 10:41:22 +0200

    Fix wrong console output in code-quality script.

--- code-quality.js
+++ code-quality.js
@@ -13,17 +13,17 @@ console.log(exec('mvn process-classes', options));
 console.log('Running "yarn ts:check" in client folder...');
 console.log(exec('cd client && yarn ts:check', options));
 
-console.log('Running "yarn ts:fix" in client folder...');
+console.log('Running "yarn eslint:fix" in client folder...');
 console.log(exec('cd client && yarn eslint:fix', options));
 
 console.log('Running "yarn ts:check" in server folder...');
 console.log(exec('cd server && yarn ts:check', options));
 
-console.log('Running "yarn ts:fix" in server folder...');
+console.log('Running "yarn eslint:fix" in server folder...');
 console.log(exec('cd server && yarn eslint:fix', options));
 
 console.log('Running "yarn ts:check" in shared folder...');
 console.log(exec('cd shared && yarn ts:check', options));
 
-console.log('Running "yarn ts:fix" in shared folder...');
+console.log('Running "yarn eslint:fix" in shared folder...');
 console.log(exec('cd shared && yarn eslint:fix', options));
