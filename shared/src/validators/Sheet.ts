c97350ed94a3ee29c91a14107c2a7e22276a0be7
Dudrie <Dudrie@gmx.de>
2019-08-29 10:38:32 +0200

    Add export if ExerciseDTOSchema.

--- shared/src/validators/Sheet.ts
+++ shared/src/validators/Sheet.ts
@@ -3,7 +3,7 @@ import { validateSchema } from './helper';
 import { ValidationErrorsWrapper } from '../model/errors/Errors';
 import { SheetDTO, ExerciseDTO } from '../model/Sheet';
 
-const ExerciseDTOSchema = Yup.object().shape<ExerciseDTO>({
+export const ExerciseDTOSchema = Yup.object().shape<ExerciseDTO>({
   exNo: Yup.number().required(),
   maxPoints: Yup.number().required(),
   bonus: Yup.boolean().required(),
