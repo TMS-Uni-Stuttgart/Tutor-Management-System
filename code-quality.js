3b4ce606df939c321588b73055b7fa112c244806
Dudrie <Dudrie@gmx.de>
2019-08-30 18:56:37 +0200

    Rename "eslint:fix" scripts to "format".

--- code-quality.js
+++ code-quality.js
@@ -13,17 +13,17 @@ console.log(exec('mvn process-classes', options));
 console.log('Running "yarn ts:check" in client folder...');
 console.log(exec('cd client && yarn ts:check', options));
 
-console.log('Running "yarn eslint:fix" in client folder...');
-console.log(exec('cd client && yarn eslint:fix', options));
+console.log('Running "yarn format" in client folder...');
+console.log(exec('cd client && yarn format', options));
 
 console.log('Running "yarn ts:check" in server folder...');
 console.log(exec('cd server && yarn ts:check', options));
 
-console.log('Running "yarn eslint:fix" in server folder...');
-console.log(exec('cd server && yarn eslint:fix', options));
+console.log('Running "yarn format" in server folder...');
+console.log(exec('cd server && yarn format', options));
 
 console.log('Running "yarn ts:check" in shared folder...');
 console.log(exec('cd shared && yarn ts:check', options));
 
-console.log('Running "yarn eslint:fix" in shared folder...');
-console.log(exec('cd shared && yarn eslint:fix', options));
+console.log('Running "yarn format" in shared folder...');
+console.log(exec('cd shared && yarn format', options));
