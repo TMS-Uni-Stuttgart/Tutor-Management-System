477c3aae23a7ae861494d589863b5d310cea4e23
Dudrie <Dudrie@gmx.de>
2019-09-03 09:10:30 +0200

    Add function to convert an AttendanceDTO to an AttendanceDoc.

--- server/src/model/documents/AttendanceDocument.ts
+++ server/src/model/documents/AttendanceDocument.ts
@@ -1,6 +1,7 @@
-import { Document } from 'mongoose';
-import { Attendance, AttendanceState } from 'shared/dist/model/Attendance';
+import { Document, Model } from 'mongoose';
+import { Attendance, AttendanceState, AttendanceDTO } from 'shared/dist/model/Attendance';
 import { prop, Typegoose } from 'typegoose';
+import { startOfDay } from 'date-fns';
 
 export class AttendanceSchema extends Typegoose implements Omit<Attendance, 'id'> {
   // TODO: Is the special ID really needed due to the fact that we now have documents which we can save as embedded documents?
@@ -19,9 +20,17 @@ export class AttendanceSchema extends Typegoose implements Omit<Attendance, 'id'
 
 export interface AttendanceDocument extends AttendanceSchema, Document {}
 
-// const AttendanceModel: Model<AttendanceDocument> = new AttendanceSchema().getModelForClass(
-//   AttendanceSchema,
-//   { schemaOptions: { collection: CollectionName.ATTENDANCE } }
-// );
+const AttendanceModel: Model<AttendanceDocument> = new AttendanceSchema().getModelForClass(
+  AttendanceSchema
+);
 
-// export default AttendanceModel;
+export function generateAttendanceDocumentFromDTO(dto: AttendanceDTO): AttendanceDocument {
+  const date = startOfDay(new Date(dto.date));
+  const attendance: Omit<AttendanceSchema, keyof Typegoose> = {
+    date,
+    state: dto.state,
+    note: dto.note,
+  };
+
+  return new AttendanceModel(attendance);
+}
