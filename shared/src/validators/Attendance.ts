ff3e6f0a3cf3df02f21315542d1ac356f74dcd7f
Dudrie <Dudrie@gmx.de>
2019-09-03 09:11:07 +0200

    Add testing 'date' in AttendanceDTO to be a valid Date.

--- shared/src/validators/Attendance.ts
+++ shared/src/validators/Attendance.ts
@@ -2,11 +2,13 @@ import * as Yup from 'yup';
 import { AttendanceDTO, AttendanceState } from '../model/Attendance';
 import { ValidationErrorsWrapper } from '../model/errors/Errors';
 import { validateSchema } from './helper';
-import { isDate } from 'date-fns';
+import { isValid } from 'date-fns';
 
 const AttendanceDTOSchema = Yup.object().shape<AttendanceDTO>({
   date: Yup.string()
-    .test({ test: value => isDate(value) })
+    .test({
+      test: value => isValid(new Date(value)),
+    })
     .required(),
   note: Yup.string(),
   state: Yup.mixed<AttendanceState | undefined>().oneOf(Object.values(AttendanceState)),
