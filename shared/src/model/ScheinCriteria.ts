5f3cc799bd408417a91bebecc2178d84ed089281
Dudrie <Dudrie@gmx.de>
2019-09-05 23:10:34 +0200

    Add ScheinCriteriaDTO to shared & rename Response.

--- shared/src/model/ScheinCriteria.ts
+++ shared/src/model/ScheinCriteria.ts
@@ -1,5 +1,12 @@
-export interface ScheinCriteria {
+export interface ScheinCriteriaResponse {
   id: string;
   identifier: string;
   name: string;
+  [key: string]: any;
+}
+
+export interface ScheinCriteriaDTO {
+  data: any;
+  name: string;
+  identifier: string;
 }
