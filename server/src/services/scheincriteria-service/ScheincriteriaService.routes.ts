7bef11be2ecb460963407e356b600bdf4da5dd71
Dudrie <Dudrie@gmx.de>
2019-09-05 13:43:06 +0200

    Add /scheincriteria/form@GET route.

--- server/src/services/scheincriteria-service/ScheincriteriaService.routes.ts
+++ server/src/services/scheincriteria-service/ScheincriteriaService.routes.ts
@@ -1,8 +1,17 @@
 import Router from 'express-promise-router';
-import { initScheincriteriaBlueprints } from '../../model/scheincriteria/scheincriterias';
+import { initScheincriteriaBlueprints } from '../../model/scheincriteria/Scheincriteria';
+import { checkRoleAccess } from '../../middleware/AccessControl';
+import { Role } from 'shared/dist/model/Role';
+import scheincriteriaService from './ScheincriteriaService.class';
 
 initScheincriteriaBlueprints();
 
 const scheincriteriaRouter = Router();
+
+scheincriteriaRouter.get('/form', ...checkRoleAccess(Role.ADMIN), async (_, res) => {
+  const formData = await scheincriteriaService.getFormData();
+
+  res.json(formData);
+});
 
 export default scheincriteriaRouter;
