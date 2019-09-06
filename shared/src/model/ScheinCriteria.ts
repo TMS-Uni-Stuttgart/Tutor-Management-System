0aa9cb4019de81f68de2395cba9fd2cdaafdc4aa
Dudrie <Dudrie@gmx.de>
2019-09-06 14:46:04 +0200

    Add types needed for evaluation of scheincriterias to shared.

--- shared/src/model/ScheinCriteria.ts
+++ shared/src/model/ScheinCriteria.ts
@@ -10,3 +10,52 @@ export interface ScheinCriteriaDTO {
   name: string;
   identifier: string;
 }
+
+export interface ScheincriteriaSummaryByStudents {
+  [studentId: string]: ScheinCriteriaSummary;
+}
+
+export interface ScheinCriteriaSummary {
+  passed: boolean;
+  scheinCriteriaSummary: { [criteriaId: string]: ScheinCriteriaResponse };
+}
+
+// TODO: Rename commentated properties to self-explanatory. This comes with changes in the frontend!
+export interface ScheinCriteriaStatus {
+  // This is actually the ID of the criteria to which the response belongs to.
+  id: string;
+
+  // This is the identifier of the criteria which the response belongs to.
+  identifier: string;
+
+  // This is the name of the criteria which the response belongs to.
+  name: string;
+  passed: boolean;
+
+  achieved: number;
+  total: number;
+  unit: ScheinCriteriaUnit;
+  infos: { [index: string]: ScheinCriteriaAdditionalStatus };
+}
+
+export interface ScheinCriteriaAdditionalStatus {
+  no: number;
+  achieved: number;
+  total: number;
+  unit: ScheinCriteriaUnit;
+  state: PassedState;
+}
+
+export enum PassedState {
+  PASSED = 'PASSED',
+  NOTPASSED = 'NOTPASSED',
+  IGNORE = 'IGNORE',
+}
+
+export enum ScheinCriteriaUnit {
+  SHEET = 'SHEET',
+  POINT = 'POINT',
+  EXAM = 'EXAM',
+  PRESENTATION = 'PRESENTATION',
+  DATE = 'DATE',
+}
