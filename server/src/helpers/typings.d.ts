100715f2b6791272f8ec6cfba82b15d47df5f730
Dudrie <Dudrie@gmx.de>
2019-09-06 14:47:10 +0200

    Fix missing exclusion of 'identifier' in helper type.

--- server/src/helpers/typings.d.ts
+++ server/src/helpers/typings.d.ts
@@ -1,5 +1,9 @@
+import { Scheincriteria } from '../model/scheincriteria/Scheincriteria';
+
 type FunctionKeys<T> = ({ [P in keyof T]: T[P] extends Function ? P : never })[keyof T];
 
 export type NoFunctions<T> = Omit<T, FunctionKeys<T>>;
 
-export type CleanShape<T, U> = NoFunctions<Omit<T, keyof U>>;
+export type CleanCriteriaShape<T, U = Scheincriteria> = NoFunctions<
+  Omit<T, keyof U | 'identifier'>
+>;
