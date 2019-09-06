6db44c9374cb39e49445afb2cd9677c6d2a7cb11
Dudrie <Dudrie@gmx.de>
2019-09-06 15:14:44 +0200

    Add checkCriteriaStatus implementation in AttendanceCriteria.

--- server/src/model/scheincriteria/criterias/AttendanceCriteria.ts
+++ server/src/model/scheincriteria/criterias/AttendanceCriteria.ts
@@ -5,6 +5,9 @@ import {
   PossiblePercentageCriteria,
   possiblePercentageCriteriaSchema,
 } from './PossiblePercentageCriteria';
+import tutorialService from '../../../services/tutorial-service/TutorialService.class';
+import { AttendanceState } from 'shared/dist/model/Attendance';
+import { ScheinCriteriaUnit } from 'shared/dist/model/ScheinCriteria';
 
 export class AttendanceCriteria extends PossiblePercentageCriteria {
   constructor(percentage: boolean, valueNeeded: number) {
@@ -12,10 +15,35 @@ export class AttendanceCriteria extends PossiblePercentageCriteria {
   }
 
   isPassed(student: Student): boolean {
+    // TODO: Remove me!
     throw new Error('Method not implemented.');
   }
 
   async checkCriteriaStatus(student: Student): Promise<StatusCheckResponse> {
+    const tutorial = await tutorialService.getDocumentWithID(student.tutorial);
+    const total = tutorial.dates.length;
+
+    let visitedOrExcused = 0;
+
+    Object.values(student.attendance).forEach(({ state }) => {
+      if (state === AttendanceState.PRESENT || state === AttendanceState.EXCUSED) {
+        visitedOrExcused += 1;
+      }
+    });
+
+    const passed: boolean = this.percentage
+      ? visitedOrExcused / total > this.valueNeeded
+      : visitedOrExcused > this.valueNeeded;
+
+    return {
+      identifier: this.identifier,
+      achieved: visitedOrExcused,
+      total,
+      passed,
+      unit: ScheinCriteriaUnit.DATE,
+      infos: {},
+    };
+
     throw new Error('Method not implemented.');
   }
 }
