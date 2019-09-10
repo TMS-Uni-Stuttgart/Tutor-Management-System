6a4e3e3d85aa0bd44773d9cbf376663841425cae
Dudrie <Dudrie@gmx.de>
2019-09-10 15:09:40 +0200

    Fix regex in YupIdShape disallowing empty string.
    An empty string is the same as no tutor given.

--- shared/src/validators/helper.ts
+++ shared/src/validators/helper.ts
@@ -29,4 +29,4 @@ export function getErrorExtracts(err: Yup.ValidationError): ValidationErrorExtra
   return errors;
 }
 
-export const YupIdShape = Yup.string().matches(/^[a-f\d]{24}$/i, 'Not a valid Object ID.');
+export const YupIdShape = Yup.string().matches(/^$|^[a-f\d]{24}$/i, 'Not a valid Object ID.');
