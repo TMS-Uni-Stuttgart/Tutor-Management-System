94f8a9291dad6acd829920a153096e97414cb53e
Dudrie <Dudrie@gmx.de>
2019-09-10 22:23:39 +0200

    Remove all Java related content.

--- src/main/java/de/unistuttgart/iste/rss/tutormanagementsystem/model/scheincriteria/PassedState.java
+++
@@ -1,9 +0,0 @@
-package de.unistuttgart.iste.rss.tutormanagementsystem.model.scheincriteria;
-
-/**
- * PassedState
- */
-public enum PassedState {
-    PASSED, NOTPASSED, IGNORE
-
-}
--- src/main/java/de/unistuttgart/iste/rss/tutormanagementsystem/model/scheincriteria/ScheinCriteriaUnit.java
+++
@@ -1,8 +0,0 @@
-package de.unistuttgart.iste.rss.tutormanagementsystem.model.scheincriteria;
-
-/**
- * CriteriaUnit
- */
-public enum ScheinCriteriaUnit {
-    SHEET, POINT, EXAM, PRESENTATION, DATE
-}
