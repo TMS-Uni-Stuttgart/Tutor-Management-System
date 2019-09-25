04128c4c91fc56d6c02cdbbaea2cffe5e8f038fe
Dudrie <Dudrie@gmx.de>
2019-09-25 20:03:31 +0200

    Add use of helperText property to FormikSelect.

--- client/src/components/forms/components/FormikSelect.tsx
+++ client/src/components/forms/components/FormikSelect.tsx
@@ -6,7 +6,7 @@ interface Props<T> extends CustomSelectProps<T> {
   name: string;
 }
 
-function FormikSelect<T>({ onChange, name, ...other }: Props<T>): JSX.Element {
+function FormikSelect<T>({ onChange, name, helperText, ...other }: Props<T>): JSX.Element {
   return (
     <Field name={name}>
       {({ field, form }: FieldProps) => (
@@ -14,7 +14,7 @@ function FormikSelect<T>({ onChange, name, ...other }: Props<T>): JSX.Element {
           {...other}
           {...field}
           onChange={onChange ? onChange : field.onChange}
-          helperText={!!form.touched[field.name] && form.errors[field.name]}
+          helperText={(!!form.touched[field.name] && form.errors[field.name]) || helperText}
           error={Boolean(form.touched[field.name]) && Boolean(form.errors[field.name])}
         />
       )}
