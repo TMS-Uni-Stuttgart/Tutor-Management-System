ec8eea83fe27d81e1e975b92f7861ad4959dbf32
Dudrie <Dudrie@gmx.de>
2019-10-30 23:06:48 +0100

    Add try-catch if no sessionTimeout config was provided.

--- server/src/helpers/config.ts
+++ server/src/helpers/config.ts
@@ -12,23 +12,31 @@ export interface DatabaseConfig {
 export const databaseConfig: DatabaseConfig = config.get('database');
 
 export function getSessionTimeout(): number {
-  const timeoutSetting = config.get('sessionTimeout');
+  try {
+    const timeoutSetting = config.get('sessionTimeout');
 
-  if (typeof timeoutSetting === 'number') {
-    return timeoutSetting;
-  }
-
-  if (typeof timeoutSetting === 'string') {
-    const timeout = Number.parseInt(timeoutSetting);
-
-    if (!Number.isNaN(timeout)) {
-      return timeout;
+    if (typeof timeoutSetting === 'number') {
+      return timeoutSetting;
     }
+
+    if (typeof timeoutSetting === 'string') {
+      const timeout = Number.parseInt(timeoutSetting);
+
+      if (!Number.isNaN(timeout)) {
+        return timeout;
+      }
+    }
+
+    Logger.warn(
+      `Invalid setting for 'sessionTimeout' provided (got: '${timeoutSetting}'). It has to be a number or a string which could be parsed to an integer. Falling back to a default value of 2h.`
+    );
+
+    return 120;
+  } catch {
+    Logger.warn(
+      "No setting for 'sessionTimeout' got provided. Falling back to the default value of 2h."
+    );
+
+    return 120;
   }
-
-  Logger.warn(
-    `Invalid setting for 'sessionTimeout' provided (got: '${timeoutSetting}'). It has to be a number or a string which could be parsed to an integer. Falling back to a default value of 2h.`
-  );
-
-  return 120;
 }
