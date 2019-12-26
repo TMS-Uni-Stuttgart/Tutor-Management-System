61e01a92b7940dde72ef477c7b8518444e3f5065
Dudrie <Dudrie@gmx.de>
2019-12-26 23:44:13 +0100

    Add SplitButton according to MUI documentation.

---
+++ client/src/view/enter-points-view/components/SplitButton.tsx
@@ -0,0 +1,100 @@
+import {
+  Button,
+  ButtonGroup,
+  Grow,
+  Popper,
+  Paper,
+  ClickAwayListener,
+  MenuList,
+  MenuItem,
+  ButtonProps,
+} from '@material-ui/core';
+import { MenuDown as ArrowDropDownIcon } from 'mdi-material-ui';
+import React, { MouseEventHandler } from 'react';
+
+interface ButtonOption {
+  label: string;
+  onClick: MouseEventHandler<HTMLButtonElement>;
+}
+
+interface Props {
+  options: ButtonOption[];
+  variant?: ButtonProps['variant'];
+  color?: ButtonProps['color'];
+}
+
+function SplitButton({ options, variant, color }: Props): JSX.Element {
+  const [open, setOpen] = React.useState(false);
+  const anchorRef = React.useRef<HTMLDivElement>(null);
+  const [selectedIndex, setSelectedIndex] = React.useState(0);
+
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    options[selectedIndex].onClick(e);
+  };
+
+  const handleMenuItemClick = (
+    event: React.MouseEvent<HTMLLIElement, MouseEvent>,
+    index: number
+  ) => {
+    setSelectedIndex(index);
+    setOpen(false);
+  };
+
+  const handleToggle = () => {
+    setOpen(prevOpen => !prevOpen);
+  };
+
+  const handleClose = (event: React.MouseEvent<Document, MouseEvent>) => {
+    if (anchorRef.current?.contains(event.target as HTMLElement)) {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  return (
+    <>
+      <ButtonGroup variant={variant} color={color} ref={anchorRef} aria-label='split button'>
+        <Button onClick={handleClick}>{options[selectedIndex].label}</Button>
+        <Button
+          size='small'
+          aria-controls={open ? 'split-button-menu' : undefined}
+          aria-expanded={open ? 'true' : undefined}
+          aria-haspopup='menu'
+          onClick={handleToggle}
+        >
+          <ArrowDropDownIcon />
+        </Button>
+      </ButtonGroup>
+
+      <Popper open={open} anchorEl={anchorRef.current} role={undefined} transition disablePortal>
+        {({ TransitionProps, placement }) => (
+          <Grow
+            {...TransitionProps}
+            style={{
+              transformOrigin: placement === 'bottom' ? 'center top' : 'center bottom',
+            }}
+          >
+            <Paper>
+              <ClickAwayListener onClickAway={handleClose}>
+                <MenuList id='split-button-menu'>
+                  {options.map((option, index) => (
+                    <MenuItem
+                      key={option.label}
+                      selected={index === selectedIndex}
+                      onClick={event => handleMenuItemClick(event, index)}
+                    >
+                      {option.label}
+                    </MenuItem>
+                  ))}
+                </MenuList>
+              </ClickAwayListener>
+            </Paper>
+          </Grow>
+        )}
+      </Popper>
+    </>
+  );
+}
+
+export default SplitButton;
