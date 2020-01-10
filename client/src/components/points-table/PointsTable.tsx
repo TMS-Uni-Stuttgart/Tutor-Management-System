c371ec16db91345d30515c950fe1b78622204c18
Dudrie <Dudrie@gmx.de>
2020-01-11 00:27:54 +0100

    Change PointsTable to be a little bit more customizable.

--- client/src/components/points-table/PointsTable.tsx
+++ client/src/components/points-table/PointsTable.tsx
@@ -7,6 +7,9 @@ import {
   TableContainerBaseProps,
   TableHead,
   TableRow,
+  PaperProps,
+  TableProps,
+  TableContainerProps,
 } from '@material-ui/core';
 import React from 'react';
 import {
@@ -17,10 +20,13 @@ import {
   PointMap,
 } from 'shared/dist/model/Points';
 import { Exercise, HasExercises } from 'shared/dist/model/Sheet';
+import clsx from 'clsx';
 
-interface Props extends React.ComponentProps<'div'> {
+interface Props extends Omit<TableContainerProps, 'ref'> {
   points: PointMap;
   sheet: HasExercises;
+  disablePaper?: boolean;
+  size?: TableProps['size'];
 }
 
 function TablePaper({ children, ...props }: TableContainerBaseProps): JSX.Element {
@@ -37,35 +43,48 @@ function getPointStringOfExercise(exercise: Exercise): string {
   return convertExercisePointInfoToString(pointInfo);
 }
 
-function PointsTable({ points, sheet, ...props }: Props): JSX.Element {
+function PointsTable({
+  points,
+  sheet,
+  size,
+  disablePaper,
+  className,
+  ...props
+}: Props): JSX.Element {
   const achieved = points.getSumOfPoints(sheet);
   const total = convertExercisePointInfoToString(getPointsOfAllExercises(sheet));
 
-  return (
-    <TableContainer component={TablePaper} {...props} ref={undefined}>
-      <Table size='small'>
-        <TableHead>
-          <TableRow>
-            <TableCell>Gesamt:</TableCell>
-            <TableCell align='right'>{achieved}</TableCell>
-            <TableCell align='left'>/ {total}</TableCell>
-          </TableRow>
-        </TableHead>
+  const TableComp = (
+    <Table size={size ?? 'small'} className={clsx(disablePaper && className)}>
+      <TableHead>
+        <TableRow>
+          <TableCell>Gesamt:</TableCell>
+          <TableCell align='right'>{achieved}</TableCell>
+          <TableCell align='left'>/ {total}</TableCell>
+        </TableRow>
+      </TableHead>
 
-        <TableBody>
-          {sheet.exercises.map(ex => (
-            <TableRow key={ex.id}>
-              <TableCell>Aufgabe {ex.exName}</TableCell>
-              <TableCell align='right'>
-                {points.getPoints(new PointId(sheet.id, ex)) ?? 0}
-              </TableCell>
-              <TableCell align='left'>/ {getPointStringOfExercise(ex)}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+      <TableBody>
+        {sheet.exercises.map(ex => (
+          <TableRow key={ex.id}>
+            <TableCell>Aufgabe {ex.exName}</TableCell>
+            <TableCell align='right'>{points.getPoints(new PointId(sheet.id, ex)) ?? 0}</TableCell>
+            <TableCell align='left'>/ {getPointStringOfExercise(ex)}</TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
   );
+
+  if (disablePaper) {
+    return <>{TableComp}</>;
+  } else {
+    return (
+      <TableContainer component={TablePaper} {...props} className={className} ref={undefined}>
+        {TableComp}
+      </TableContainer>
+    );
+  }
 }
 
 export default PointsTable;
