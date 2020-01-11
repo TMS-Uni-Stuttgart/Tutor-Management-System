af50475ab515e2f10c13b2798ef1d8e8cb52eb5c
Dudrie <Dudrie@gmx.de>
2020-01-11 18:32:59 +0100

    Add CriteriaChart component for the StudentInfo view.

--- client/src/view/studentmanagement/student-info/StudentInfo.tsx
+++ client/src/view/studentmanagement/student-info/StudentInfo.tsx
@@ -1,9 +1,8 @@
-import { Box, CircularProgress, Grid, Paper, Typography } from '@material-ui/core';
+import { Box, CircularProgress, Typography } from '@material-ui/core';
 import { createStyles, makeStyles, useTheme } from '@material-ui/core/styles';
 import clsx from 'clsx';
 import { useSnackbar } from 'notistack';
 import React, { useEffect, useState } from 'react';
-import Chart from 'react-google-charts';
 import { useParams } from 'react-router';
 import { ScheinCriteriaSummary } from 'shared/dist/model/ScheinCriteria';
 import { Student } from 'shared/dist/model/Student';
@@ -13,6 +12,7 @@ import Placeholder from '../../../components/Placeholder';
 import { getScheinCriteriaSummaryOfStudent, getStudent } from '../../../hooks/fetching/Student';
 import { useErrorSnackbar } from '../../../hooks/useErrorSnackbar';
 import { getStudentOverviewPath } from '../../../routes/Routing.helpers';
+import CriteriaCharts from './components/CriteriaCharts';
 import EvaluationInformation from './components/EvaluationInformation';
 
 const useStyles = makeStyles(theme =>
@@ -30,25 +30,8 @@ const useStyles = makeStyles(theme =>
     redFont: {
       color: theme.palette.red.main,
     },
-    criteriaGrid: {
-      marginBottom: theme.spacing(2),
-    },
-    summaryPaper: {
-      flex: 1,
-      background: theme.palette.background.default,
-      padding: theme.spacing(1.5),
-    },
-    title: {
-      textAlign: 'center',
-    },
-    chart: {
-      width: '100%',
-      height: '100%',
-    },
-    loader: {
-      position: 'absolute',
-      top: '50%',
-      left: '50%',
+    criteriaCharts: {
+      marginBottom: theme.spacing(1),
     },
     evaluationInfo: {
       marginBottom: theme.spacing(1),
@@ -63,6 +46,8 @@ interface RouteParams {
 
 function StudentInfo(): JSX.Element {
   const classes = useStyles();
+  const theme = useTheme();
+
   const { studentId, tutorialId } = useParams<RouteParams>();
 
   const { enqueueSnackbar } = useSnackbar();
@@ -70,9 +55,6 @@ function StudentInfo(): JSX.Element {
 
   const [student, setStudent] = useState<Student>();
   const [scheinStatus, setScheinStatus] = useState<ScheinCriteriaSummary>();
-
-  const theme = useTheme();
-  const { fontStyle } = theme.mixins.chart(theme);
 
   useEffect(() => {
     getStudent(studentId)
@@ -143,38 +125,9 @@ function StudentInfo(): JSX.Element {
         loading={!student}
       >
         <Box display='flex' flexDirection='column'>
-          <Grid container spacing={2} className={classes.criteriaGrid}>
-            {scheinStatus &&
-              Object.values(scheinStatus.scheinCriteriaSummary).map(summary => (
-                <Grid item key={summary.id} sm={12} md={6} lg={4}>
-                  <Paper key={summary.id} variant='outlined' className={classes.summaryPaper}>
-                    <Typography className={classes.title}>{summary.name}</Typography>
-
-                    <Chart
-                      className={classes.chart}
-                      chartType='PieChart'
-                      loader={<CircularProgress className={classes.loader} />}
-                      data={[
-                        ['Status', 'Anzahl'],
-                        ['Erfüllt', summary.achieved],
-                        ['Nicht erfüllt', summary.total],
-                      ]}
-                      options={{
-                        backgroundColor: 'transparent',
-                        ...fontStyle,
-                        slices: {
-                          0: { color: theme.palette.green.dark },
-                          1: { color: theme.palette.red.dark },
-                        },
-                        legend: {
-                          ...fontStyle,
-                        },
-                      }}
-                    />
-                  </Paper>
-                </Grid>
-              ))}
-          </Grid>
+          {scheinStatus && (
+            <CriteriaCharts scheinStatus={scheinStatus} className={classes.criteriaCharts} />
+          )}
 
           {student && (
             <EvaluationInformation student={student} className={classes.evaluationInfo} />
---
+++ client/src/view/studentmanagement/student-info/components/CriteriaCharts.tsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import { makeStyles, createStyles, useTheme } from '@material-ui/core/styles';
+import { Grid, Paper, Typography, CircularProgress, GridProps } from '@material-ui/core';
+import Chart from 'react-google-charts';
+import { ScheinCriteriaSummary } from 'shared/dist/model/ScheinCriteria';
+
+const useStyles = makeStyles(theme =>
+  createStyles({
+    summaryPaper: {
+      flex: 1,
+      background: theme.palette.background.default,
+      padding: theme.spacing(1.5),
+    },
+    title: {
+      textAlign: 'center',
+    },
+    chart: {
+      width: '100%',
+      height: '100%',
+    },
+    loader: {
+      position: 'absolute',
+      top: '50%',
+      left: '50%',
+    },
+  })
+);
+
+interface Props extends GridProps {
+  scheinStatus: ScheinCriteriaSummary;
+}
+
+function CriteriaCharts({ scheinStatus, ...props }: Props): JSX.Element {
+  const classes = useStyles();
+  const theme = useTheme();
+  const { fontStyle } = theme.mixins.chart(theme);
+
+  return (
+    <Grid container spacing={2} {...props}>
+      {Object.values(scheinStatus.scheinCriteriaSummary).map(summary => (
+        <Grid item key={summary.id} sm={12} md={6} lg={4}>
+          <Paper key={summary.id} variant='outlined' className={classes.summaryPaper}>
+            <Typography className={classes.title}>{summary.name}</Typography>
+
+            <Chart
+              className={classes.chart}
+              chartType='PieChart'
+              loader={<CircularProgress className={classes.loader} />}
+              data={[
+                ['Status', 'Anzahl'],
+                ['Erfüllt', summary.achieved],
+                ['Nicht erfüllt', summary.total],
+              ]}
+              options={{
+                backgroundColor: 'transparent',
+                ...fontStyle,
+                slices: {
+                  0: { color: theme.palette.green.dark },
+                  1: { color: theme.palette.red.dark },
+                },
+                legend: {
+                  ...fontStyle,
+                },
+              }}
+            />
+          </Paper>
+        </Grid>
+      ))}
+    </Grid>
+  );
+}
+
+export default CriteriaCharts;
