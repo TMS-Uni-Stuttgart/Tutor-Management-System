e0b3c37814e56fcfb9f6573c7d2b19ae300ab7e6
Dudrie <Dudrie@gmx.de>
2020-01-24 01:18:23 +0100

    Change small parts of ScheinexamDoc & StudentDoc.

--- server/src/model/documents/ScheinexamDocument.ts
+++ server/src/model/documents/ScheinexamDocument.ts
@@ -1,10 +1,16 @@
 import { arrayProp, instanceMethod, InstanceType, prop, Typegoose } from '@typegoose/typegoose';
 import { Document, Model } from 'mongoose';
 import { ScheinExam } from 'shared/dist/model/Scheinexam';
-import { getPointsOfAllExercises, PointMap } from 'shared/dist/model/Points';
+import { getPointsOfAllExercises, PointMap, ExercisePointInfo } from 'shared/dist/model/Points';
 import { CollectionName } from '../CollectionName';
 import { ExerciseDocument, ExerciseSchema } from './ExerciseDocument';
 import { StudentDocument } from './StudentDocument';
+
+interface PassedInformation {
+  passed: boolean;
+  achieved: number;
+  total: ExercisePointInfo;
+}
 
 export class ScheinexamSchema extends Typegoose implements Omit<ScheinExam, 'id'> {
   @prop({ required: true })
@@ -20,12 +26,16 @@ export class ScheinexamSchema extends Typegoose implements Omit<ScheinExam, 'id'
   percentageNeeded!: number;
 
   @instanceMethod
-  hasPassed(this: InstanceType<ScheinexamSchema>, student: StudentDocument): boolean {
+  hasPassed(this: InstanceType<ScheinexamSchema>, student: StudentDocument): PassedInformation {
     const points = new PointMap(student.scheinExamResults);
     const achieved: number = points.getSumOfPoints(this);
-    const { must: total } = getPointsOfAllExercises(this);
+    const { must, bonus } = getPointsOfAllExercises(this);
 
-    return achieved / total >= this.percentageNeeded;
+    return {
+      passed: achieved / must >= this.percentageNeeded,
+      achieved,
+      total: { must, bonus },
+    };
   }
 }
 
