84b16a4c6b3a407a3349597e767e6e8680312a8d
Dudrie <Dudrie@gmx.de>
2020-02-16 16:44:14 +0100

    Add tests for TutorialService::findAll.

--- server/src/module/tutorial/tutorial.controller.spec.ts
+++ server/src/module/tutorial/tutorial.controller.spec.ts
@@ -1,5 +1,10 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TutorialController } from './tutorial.controller';
+import { TutorialService } from './tutorial.service';
+import { mocked } from 'ts-jest/utils';
+
+jest.mock('./tutorial.service');
+const mockedTutorialService = mocked(TutorialService, true);
 
 describe('Tutorial Controller', () => {
   let controller: TutorialController;
@@ -7,6 +12,7 @@ describe('Tutorial Controller', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [TutorialController],
+      providers: [{ provide: TutorialService, useValue: mockedTutorialService }],
     }).compile();
 
     controller = module.get<TutorialController>(TutorialController);
