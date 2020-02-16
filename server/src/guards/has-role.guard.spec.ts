accb864e78056da168edfc317b75054a90d1221f
Dudrie <Dudrie@gmx.de>
2020-02-16 14:19:45 +0100

    Fix failing test for HasRoleGuard.

--- server/src/guards/has-role.guard.spec.ts
+++ server/src/guards/has-role.guard.spec.ts
@@ -1,7 +1,8 @@
 import { HasRoleGuard } from './has-role.guard';
+import { Role } from '../shared/model/Role';
 
 describe('HasRoleGuard', () => {
   it('should be defined', () => {
-    expect(new HasRoleGuard()).toBeDefined();
+    expect(new HasRoleGuard(Role.ADMIN)).toBeDefined();
   });
 });
