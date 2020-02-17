553cfbc9ae43fe1ba58c9b080edbbe598830a917
Dudrie <Dudrie@gmx.de>
2020-02-17 20:13:35 +0100

    Fix errors in tests.

--- server/src/module/student/student.controller.spec.ts
+++ server/src/module/student/student.controller.spec.ts
@@ -1,11 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { StudentController } from './student.controller';
+import { StudentService } from './student.service';
+import { MockedStudentService } from '../../../test/mocks/student.service.mock';
 
 describe('Student Controller', () => {
   let controller: StudentController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      providers: [{ provide: StudentService, useClass: MockedStudentService }],
       controllers: [StudentController],
     }).compile();
 
