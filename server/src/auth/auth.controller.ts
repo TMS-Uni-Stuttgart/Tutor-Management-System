87e0197fd0b69650dd160c86150530c3ea0a42f7
Dudrie <Dudrie@gmx.de>
2020-02-20 16:42:06 +0100

    Change /login@POST response code to 200 instead of 201.

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,10 +1,11 @@
-import { Controller, Post, UseGuards, Req, Get, Res } from '@nestjs/common';
+import { Controller, Post, UseGuards, Req, Get, Res, HttpCode } from '@nestjs/common';
 import { LoginGuard } from '../guards/login.guard';
 import { Request, Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
   @Post('/login')
+  @HttpCode(200)
   @UseGuards(LoginGuard)
   login(@Req() req: Request & { user?: any }) {
     // TODO: Return correct user data.
