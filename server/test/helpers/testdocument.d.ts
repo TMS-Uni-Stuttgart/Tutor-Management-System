e9ccaf0e0c597a12dcaba52c63d49602c5acc281
Dudrie <Dudrie@gmx.de>
2020-02-21 18:41:26 +0100

    Add inserting initial documents into test collections.

--- server/test/helpers/testdocument.d.ts
+++ server/test/helpers/testdocument.d.ts
@@ -1,5 +1,4 @@
 import { NoFunctions } from '../../src/helpers/NoFunctions';
 
 export type TestDocument<M> = NoFunctions<M> & { id: string };
-
-export type MockedModel<M> = M & { _id: string };
+export type MockedModel<M> = NoFunctions<M> & { _id: string };
