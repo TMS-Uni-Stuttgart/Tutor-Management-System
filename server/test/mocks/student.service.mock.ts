a934b670df1215518dfeef7ace8caf346e52e4aa
Dudrie <Dudrie@gmx.de>
2020-02-22 12:32:09 +0100

    Remove not needed controller test files.

--- server/test/mocks/student.service.mock.ts
+++
@@ -1,6 +0,0 @@
-import { TestDocument } from '../helpers/testdocument';
-import { StudentModel } from '../../src/module/models/student.model';
-
-const STUDENT_DOCUMENTS: readonly TestDocument<StudentModel>[] = [];
-
-export class MockedStudentService {}
