06213a52e5b7a04610d84cc8fcadb8da98f01efa
Dudrie <Dudrie@gmx.de>
2020-02-26 09:40:00 +0100

    Add /scheinexam routes.

--- server/src/module/scheinexam/scheinexam.controller.ts
+++ server/src/module/scheinexam/scheinexam.controller.ts
@@ -1,4 +1,66 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  UseGuards,
+  Body,
+  Post,
+  UsePipes,
+  ValidationPipe,
+  Param,
+  Patch,
+  Delete,
+  HttpCode,
+  HttpStatus,
+} from '@nestjs/common';
+import { ScheinexamService } from './scheinexam.service';
+import { AuthenticatedGuard } from '../../guards/authenticated.guard';
+import { ScheinExam } from '../../shared/model/Scheinexam';
+import { ScheinExamDTO } from './scheinexam.dto';
+import { HasRoleGuard } from '../../guards/has-role.guard';
+import { Role } from '../../shared/model/Role';
 
 @Controller('scheinexam')
-export class ScheinexamController {}
+export class ScheinexamController {
+  constructor(private readonly scheinexamService: ScheinexamService) {}
+
+  @Get()
+  @UseGuards(AuthenticatedGuard)
+  async getAllScheinexams(): Promise<ScheinExam[]> {
+    const scheinexams = await this.scheinexamService.findAll();
+
+    return scheinexams;
+  }
+
+  @Post()
+  @UseGuards(new HasRoleGuard(Role.ADMIN))
+  @UsePipes(ValidationPipe)
+  async createScheinexam(@Body() dto: ScheinExamDTO): Promise<ScheinExam> {
+    const scheinexam = await this.scheinexamService.create(dto);
+
+    return scheinexam;
+  }
+
+  @Get('/:id')
+  @UseGuards(AuthenticatedGuard)
+  async getScheinexam(@Param('id') id: string): Promise<ScheinExam> {
+    const scheinexam = await this.scheinexamService.findById(id);
+
+    return scheinexam.toDTO();
+  }
+
+  @Patch('/:id')
+  @UseGuards(new HasRoleGuard(Role.ADMIN))
+  @UsePipes(ValidationPipe)
+  async updateScheinexam(@Param('id') id: string, @Body() dto: ScheinExamDTO): Promise<ScheinExam> {
+    const scheinexam = await this.scheinexamService.update(id, dto);
+
+    return scheinexam;
+  }
+
+  @Delete('/:id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  @UseGuards(new HasRoleGuard(Role.ADMIN))
+  async deleteScheinexam(@Param('id') id: string): Promise<void> {
+    await this.scheinexamService.delete(id);
+  }
+}
