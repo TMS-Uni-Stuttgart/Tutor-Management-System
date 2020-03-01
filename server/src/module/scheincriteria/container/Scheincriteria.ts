3cf2fdf06e7fa751b5acad39aa5a2f3cd695b9ce
Dudrie <Dudrie@gmx.de>
2020-03-01 15:47:38 +0100

    Move the Scheincriteria generation into the Scheincriteria class.
    This just makes more sense.

--- server/src/module/scheincriteria/container/Scheincriteria.ts
+++ server/src/module/scheincriteria/container/Scheincriteria.ts
@@ -1,8 +1,11 @@
-import { IsString } from 'class-validator';
+import { IsString, validateSync } from 'class-validator';
 import { ScheinexamDocument } from '../../../database/models/scheinexam.model';
 import { SheetDocument } from '../../../database/models/sheet.model';
 import { StudentDocument } from '../../../database/models/student.model';
 import { CriteriaInformation, ScheinCriteriaStatus } from '../../../shared/model/ScheinCriteria';
+import { ScheinCriteriaDTO } from '../scheincriteria.dto';
+import { ScheincriteriaContainer } from './scheincriteria.container';
+import { BadRequestException } from '@nestjs/common';
 
 export interface CriteriaPayload {
   student: StudentDocument;
@@ -25,6 +28,34 @@ export abstract class Scheincriteria {
 
   constructor(identifier: string) {
     this.identifier = identifier;
+  }
+
+  /**
+   * Generates a Scheincriteria from the given dto.
+   *
+   * The returned criteria will be an instance of the class which was saved as blueprint with the given `identifier`. The given `data` will be validated against said class before creating an instancen of it.
+   *
+   * @param dto DTO containing the information for the criteria.
+   *
+   * @returns Created scheincriteria from the dto.
+   *
+   * @throws `NotFoundException` - If no blue print of the given identifier could be found.
+   * @throws `BadRequestException` - If the data is not successfully validated against the criteria class to use.
+   */
+  static fromDTO({ identifier, data }: ScheinCriteriaDTO): Scheincriteria {
+    const bluePrintData = ScheincriteriaContainer.getContainer().getBluePrint(identifier);
+
+    // Get the constructor of the blueprint. The type needs to be set here because 'constructor' is only typed as 'Function' and therefore cannot be used with 'new' in front of it.
+    const prototype = bluePrintData.blueprint.constructor as new () => Scheincriteria;
+    const criteria: Scheincriteria = Object.assign(new prototype(), data);
+
+    const errors = validateSync(criteria, { whitelist: true, forbidNonWhitelisted: true });
+
+    if (errors.length > 0) {
+      throw new BadRequestException(errors);
+    }
+
+    return criteria;
   }
 
   abstract checkCriteriaStatus(payload: CriteriaPayload): StatusCheckResponse;
