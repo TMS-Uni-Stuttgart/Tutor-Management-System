86ea746e720a48342731ddd338dd37568ffeae3a
Dudrie <Dudrie@gmx.de>
2020-03-01 10:08:04 +0100

    Change date saving in AttendanceModel.
    Now, dates are getting saved as strings to the DB and they are being accessed through virtual getters & setters.

--- server/src/database/models/attendance.model.ts
+++ server/src/database/models/attendance.model.ts
@@ -1,16 +1,55 @@
-import { DocumentType, modelOptions, prop } from '@typegoose/typegoose';
-import { AttendanceState } from '../../shared/model/Attendance';
+import { DocumentType, modelOptions, prop, getModelForClass } from '@typegoose/typegoose';
+import { DateTime } from 'luxon';
+import { AttendanceState, Attendance } from '../../shared/model/Attendance';
+import { AttendanceDTO } from '../../module/student/student.dto';
+
+interface ConstructorFields {
+  date: Date | string;
+  note?: string;
+  state?: AttendanceState;
+}
 
 @modelOptions({})
 export class AttendanceModel {
+  constructor({ date, note, state }: ConstructorFields) {
+    this.date = date instanceof Date ? DateTime.fromJSDate(date) : DateTime.fromISO(date);
+    this.note = note;
+    this.state = state;
+  }
+
   @prop({ required: true })
-  date!: Date;
+  private _date!: string;
+
+  get date(): DateTime {
+    return DateTime.fromISO(this._date);
+  }
+
+  set date(date: DateTime) {
+    this._date = date.toISODate();
+  }
 
   @prop()
   note?: string;
 
   @prop({ enum: AttendanceState })
   state?: AttendanceState;
+
+  static fromDTO(dto: AttendanceDTO): AttendanceDocument {
+    const model = getModelForClass(AttendanceModel);
+    const attendance = new AttendanceModel(dto);
+
+    return new model(attendance);
+  }
+
+  toDTO(this: AttendanceDocument): Attendance {
+    const { date, note, state } = this;
+
+    return {
+      date: date.toISODate(),
+      note,
+      state,
+    };
+  }
 }
 
 export type AttendanceDocument = DocumentType<AttendanceModel>;
