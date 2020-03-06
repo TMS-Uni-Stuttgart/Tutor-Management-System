aa838ff29d67cf3ee4ef732315b478cb1aa81454
Dudrie <Dudrie@gmx.de>
2020-03-06 14:56:56 +0100

    Add returning the data of the logged in user.
    This affects the returned object of /auth/login.

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,16 +1,32 @@
-import { Controller, Post, UseGuards, Req, Get, Res, HttpCode, HttpStatus } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  UseGuards,
+  Req,
+  Get,
+  Res,
+  HttpCode,
+  HttpStatus,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { LoginGuard } from '../guards/login.guard';
 import { Request, Response } from 'express';
+import { ILoggedInUser } from '../shared/model/User';
+import { UserService } from '../module/user/user.service';
 
 @Controller('auth')
 export class AuthController {
+  constructor(private readonly userService: UserService) {}
+
   @Post('/login')
   @HttpCode(HttpStatus.OK)
   @UseGuards(LoginGuard)
-  login(@Req() req: Request & { user?: any }) {
-    // TODO: Return correct user data.
+  async login(@Req() req: Request): Promise<ILoggedInUser> {
+    if (!req.user) {
+      throw new UnauthorizedException();
+    }
 
-    return req.user;
+    return await this.userService.getLoggedInUserInformation(req.user._id);
   }
 
   @Get('/logout')
