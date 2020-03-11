9b6d53b559501ac984435e8279ac7bcc133b3d9c
Dudrie <Dudrie@gmx.de>
2020-03-11 22:09:41 +0100

    Add changed route file.
    That file was not staged in the last commit.

--- client/src/routes/Routing.routes.tsx
+++ client/src/routes/Routing.routes.tsx
@@ -1,16 +1,17 @@
 import { SvgIconProps } from '@material-ui/core/SvgIcon';
 import {
-  Account as PersonIcon,
-  AccountBadge as AccountBadgeIcon,
-  AccountMultipleCheck as AccountMultipleCheckIcon,
-  Book as BookIcon,
-  File as FileIcon,
-  FileDocumentBox as FileDocumentBoxIcon,
-  FileDocumentBoxMultiple as FileDocumentBoxMultipleIcon,
-  AccountMultiple as GroupIcon,
+  Account as StudentIcon,
+  AccountBadge as UserIcon,
+  AccountMultiple as TeamIcon,
+  AccountMultipleCheck as AttendancesIcon,
+  Book as EnterPointsIcon,
+  Comment as PresentationIcon,
+  File as SheetIcon,
+  FileDocumentBox as ScheinexamPointsIcon,
+  FileDocumentBoxMultiple as ScheinexamManagementIcon,
   Login as LoginIcon,
-  ScriptText as ScriptTextIcon,
-  Teach as TeachIcon,
+  ScriptText as ScheincriteriaIcon,
+  Teach as TutorialIcon,
   ViewDashboard as DashboardIcon,
 } from 'mdi-material-ui';
 import React from 'react';
@@ -18,24 +19,25 @@ import { RouteComponentProps } from 'react-router';
 import { Role } from 'shared/model/Role';
 import AttendanceAdminView from '../view/attendance/AttendanceAdminView';
 import AttendanceView from '../view/attendance/AttendanceView';
+import CriteriaInfoView from '../view/criteria-info-view/CriteriaInfoView';
 import Dashboard from '../view/dashboard/Dashboard';
 import Login from '../view/Login';
+import EnterScheinexamPoints from '../view/points-scheinexam/enter-form/EnterScheinexamPoints';
+import ScheinexamPointsOverview from '../view/points-scheinexam/overview/ScheinexamPointsOverview';
 import EnterStudentPoints from '../view/points-sheet/enter-form/EnterStudentPoints';
 import EnterTeamPoints from '../view/points-sheet/enter-form/EnterTeamPoints';
 import PointsOverview from '../view/points-sheet/overview/PointsOverview';
-import ScheinExamManagement from '../view/scheinexam-management/ScheinExamManagement';
+import PresentationPoints from '../view/presentation-points/PresentationPoints';
 import ScheinCriteriaManagement from '../view/scheincriteriamanagement/ScheinCriteriaManagement';
+import ScheinExamManagement from '../view/scheinexam-management/ScheinExamManagement';
 import SheetManagement from '../view/sheetmanagement/SheetManagement';
 import AllStudentsAdminView from '../view/studentmanagement/AllStudentsAdminView';
+import StudentInfo from '../view/studentmanagement/student-info/StudentInfo';
 import TutorStudentmanagement from '../view/studentmanagement/TutorStudentmanagement';
 import Teamoverview from '../view/teamoverview/Teamoverview';
 import TutorialManagement from '../view/tutorialmanagement/TutorialManagement';
 import TutorialSubstituteManagement from '../view/tutorialmanagement/TutorialSubstituteManagement';
 import UserManagement from '../view/usermanagement/UserManagement';
-import ScheinexamPointsOverview from '../view/points-scheinexam/overview/ScheinexamPointsOverview';
-import EnterScheinexamPoints from '../view/points-scheinexam/enter-form/EnterScheinexamPoints';
-import StudentInfo from '../view/studentmanagement/student-info/StudentInfo';
-import CriteriaInfoView from '../view/criteria-info-view/CriteriaInfoView';
 
 export enum RoutingPath {
   ROOT = '/',
@@ -47,6 +49,7 @@ export enum RoutingPath {
   ENTER_POINTS_OVERVIEW = '/enterpoints/:sheetId?',
   ENTER_POINTS_TEAM = '/enterpoints/:sheetId/team/:teamId',
   ENTER_POINTS_STUDENT = '/enterpoints/:sheetId/team/:teamId/student/:studentId',
+  PRESENTATION_POINTS = '/presentations/:sheetId?',
   SCHEIN_EXAMS_OVERVIEW = '/scheinexams/:examId?',
   SCHEIN_EXAMS_STUDENT = '/scheinexams/:examId/student/:studentId',
   DASHBOARD = '/dashboard',
@@ -102,7 +105,7 @@ export const ROUTES: readonly RouteType[] = [
     path: RoutingPath.ATTENDANCE,
     title: 'Anwesenheiten',
     component: AttendanceView,
-    icon: AccountMultipleCheckIcon,
+    icon: AttendancesIcon,
     roles: [Role.TUTOR],
     isInDrawer: true,
     isPrivate: true,
@@ -113,7 +116,7 @@ export const ROUTES: readonly RouteType[] = [
     path: RoutingPath.ENTER_POINTS_STUDENT,
     title: 'Punkte eintragen',
     component: EnterStudentPoints,
-    icon: BookIcon,
+    icon: EnterPointsIcon,
     roles: [Role.TUTOR, Role.CORRECTOR],
     isInDrawer: false,
     isPrivate: true,
@@ -123,7 +126,7 @@ export const ROUTES: readonly RouteType[] = [
     path: RoutingPath.ENTER_POINTS_TEAM,
     title: 'Punkte eintragen',
     component: EnterTeamPoints,
-    icon: BookIcon,
+    icon: EnterPointsIcon,
     roles: [Role.TUTOR, Role.CORRECTOR],
     isInDrawer: false,
     isPrivate: true,
@@ -133,17 +136,28 @@ export const ROUTES: readonly RouteType[] = [
     path: RoutingPath.ENTER_POINTS_OVERVIEW,
     title: 'Punkte verwalten',
     component: PointsOverview,
-    icon: BookIcon,
+    icon: EnterPointsIcon,
     roles: [Role.TUTOR, Role.CORRECTOR],
     isInDrawer: true,
     isPrivate: true,
     isTutorialRelated: true,
   },
   {
+    path: RoutingPath.PRESENTATION_POINTS,
+    title: 'Präsentationen',
+    component: PresentationPoints,
+    icon: PresentationIcon,
+    roles: [Role.TUTOR],
+    isInDrawer: true,
+    isPrivate: true,
+    isTutorialRelated: true,
+    isAccessibleBySubstitute: true,
+  },
+  {
     path: RoutingPath.SCHEIN_EXAMS_STUDENT,
     title: 'Scheinklausuren',
     component: EnterScheinexamPoints,
-    icon: FileDocumentBoxIcon,
+    icon: ScheinexamPointsIcon,
     roles: [Role.TUTOR, Role.CORRECTOR],
     isInDrawer: false,
     isPrivate: true,
@@ -153,7 +167,7 @@ export const ROUTES: readonly RouteType[] = [
     path: RoutingPath.SCHEIN_EXAMS_OVERVIEW,
     title: 'Scheinklausuren',
     component: ScheinexamPointsOverview,
-    icon: FileDocumentBoxIcon,
+    icon: ScheinexamPointsIcon,
     roles: [Role.TUTOR, Role.CORRECTOR],
     isInDrawer: true,
     isPrivate: true,
@@ -163,7 +177,7 @@ export const ROUTES: readonly RouteType[] = [
     path: RoutingPath.STUDENT_INFO,
     title: 'Studierendeninformation',
     component: StudentInfo,
-    icon: PersonIcon,
+    icon: StudentIcon,
     roles: [Role.TUTOR, Role.ADMIN],
     isInDrawer: false,
     isPrivate: true,
@@ -173,7 +187,7 @@ export const ROUTES: readonly RouteType[] = [
     path: RoutingPath.STUDENTOVERVIEW,
     title: 'Studierendenübersicht',
     component: TutorStudentmanagement,
-    icon: PersonIcon,
+    icon: StudentIcon,
     roles: [Role.TUTOR],
     isInDrawer: true,
     isPrivate: true,
@@ -183,7 +197,7 @@ export const ROUTES: readonly RouteType[] = [
     path: RoutingPath.TEAMOVERVIEW,
     title: 'Teamübersicht',
     component: Teamoverview,
-    icon: GroupIcon,
+    icon: TeamIcon,
     roles: [Role.TUTOR],
     isInDrawer: true,
     isPrivate: true,
@@ -193,7 +207,7 @@ export const ROUTES: readonly RouteType[] = [
     path: RoutingPath.MANAGE_USERS,
     title: 'Nutzerverwaltung',
     component: UserManagement,
-    icon: AccountBadgeIcon,
+    icon: UserIcon,
     roles: [Role.ADMIN],
     isInDrawer: true,
     isPrivate: true,
@@ -202,7 +216,7 @@ export const ROUTES: readonly RouteType[] = [
     path: RoutingPath.MANAGE_TUTORIALS_SUBSTITUTES,
     title: 'Tutorienvertretungen',
     component: TutorialSubstituteManagement,
-    icon: TeachIcon,
+    icon: TutorialIcon,
     roles: [Role.ADMIN, Role.EMPLOYEE],
     isInDrawer: false,
     isPrivate: true,
@@ -211,7 +225,7 @@ export const ROUTES: readonly RouteType[] = [
     path: RoutingPath.MANAGE_TUTORIALS,
     title: 'Tutorienverwaltung',
     component: TutorialManagement,
-    icon: TeachIcon,
+    icon: TutorialIcon,
     roles: [Role.ADMIN, Role.EMPLOYEE],
     isInDrawer: true,
     isPrivate: true,
@@ -221,7 +235,7 @@ export const ROUTES: readonly RouteType[] = [
     path: RoutingPath.MANAGE_ALL_STUDENTS,
     title: 'Studierendenübersicht',
     component: AllStudentsAdminView,
-    icon: PersonIcon,
+    icon: StudentIcon,
     roles: [Role.ADMIN],
     isInDrawer: true,
     isPrivate: true,
@@ -230,7 +244,7 @@ export const ROUTES: readonly RouteType[] = [
     path: RoutingPath.SCHEIN_CRITERIAS_INFO,
     title: 'Scheinkriterien',
     component: CriteriaInfoView,
-    icon: ScriptTextIcon,
+    icon: ScheincriteriaIcon,
     roles: [Role.ADMIN, Role.EMPLOYEE],
     isInDrawer: false,
     isPrivate: true,
@@ -239,7 +253,7 @@ export const ROUTES: readonly RouteType[] = [
     path: RoutingPath.MANAGE_SCHEIN_CRITERIAS,
     title: 'Scheinkriterien',
     component: ScheinCriteriaManagement,
-    icon: ScriptTextIcon,
+    icon: ScheincriteriaIcon,
     roles: [Role.ADMIN, Role.EMPLOYEE],
     isInDrawer: true,
     isPrivate: true,
@@ -248,7 +262,7 @@ export const ROUTES: readonly RouteType[] = [
     path: RoutingPath.MANAGE_SHEETS,
     title: 'Übungsblätter',
     component: SheetManagement,
-    icon: FileIcon,
+    icon: SheetIcon,
     roles: [Role.ADMIN, Role.EMPLOYEE],
     isInDrawer: true,
     isPrivate: true,
@@ -257,7 +271,7 @@ export const ROUTES: readonly RouteType[] = [
     path: RoutingPath.MANAGE_ATTENDANCES,
     title: 'Anwesenheiten',
     component: AttendanceAdminView,
-    icon: AccountMultipleCheckIcon,
+    icon: AttendancesIcon,
     roles: [Role.ADMIN, Role.EMPLOYEE],
     isInDrawer: true,
     isPrivate: true,
@@ -266,7 +280,7 @@ export const ROUTES: readonly RouteType[] = [
     path: RoutingPath.MANAGE_SCHEIN_EXAMS,
     title: 'Scheinklausuren',
     component: ScheinExamManagement,
-    icon: FileDocumentBoxMultipleIcon,
+    icon: ScheinexamManagementIcon,
     roles: [Role.ADMIN, Role.EMPLOYEE],
     isInDrawer: true,
     isPrivate: true,
