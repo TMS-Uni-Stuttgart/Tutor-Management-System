5c64b8194184fc2c30c3ba04ec6e4f11d4dd7fad
Dudrie <Dudrie@gmx.de>
2020-03-12 06:59:11 +0100

    Move getSubItems into TutorialRailItem.helpers.

---
+++ client/src/view/navigation-rail/components/TutorialRailItem.helpers.tsx
@@ -0,0 +1,45 @@
+import {
+  AccountConvert as SubstituteTutorialIcon,
+  CheckboxMarkedCircleOutline as TutorialToCorrectIcon,
+  Teach as TutorialIcon,
+} from 'mdi-material-ui';
+import { Role } from '../../../../../server/src/shared/model/Role';
+import { LoggedInUser } from '../../../model/LoggedInUser';
+import { Tutorial } from '../../../model/Tutorial';
+import { getTutorialRelatedPath } from '../../../routes/Routing.helpers';
+import { RouteType } from '../../../routes/Routing.routes';
+import { RailSubItemProps } from './RailSubItem';
+
+export function getSubItems(route: RouteType, userData: LoggedInUser): RailSubItemProps[] {
+  const subItems: RailSubItemProps[] = [];
+
+  userData.tutorials.forEach(tutorial => {
+    subItems.push({
+      subPath: getTutorialRelatedPath(route, tutorial.id),
+      icon: TutorialIcon,
+      text: Tutorial.getDisplayString(tutorial),
+    });
+  });
+
+  if (route.roles.includes(Role.CORRECTOR)) {
+    userData.tutorialsToCorrect.forEach(tutorial => {
+      subItems.push({
+        subPath: getTutorialRelatedPath(route, tutorial.id),
+        icon: TutorialToCorrectIcon,
+        text: Tutorial.getDisplayString(tutorial),
+      });
+    });
+  }
+
+  if (route.isAccessibleBySubstitute) {
+    userData.substituteTutorials.forEach(tutorial => {
+      subItems.push({
+        subPath: getTutorialRelatedPath(route, tutorial.id),
+        icon: SubstituteTutorialIcon,
+        text: Tutorial.getDisplayString(tutorial),
+      });
+    });
+  }
+
+  return subItems;
+}
