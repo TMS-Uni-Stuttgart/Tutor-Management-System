4359b9de7313ca8627aa9e3f679f8e149fd00f9f
Dudrie <Dudrie@gmx.de>
2020-05-18 22:49:30 +0200

    Add first steps to support dynamic route prefixes on the client.
    This adds some alteration to the index.html before this file gets served to the client.

--- server/src/filter/not-found-exception.filter.ts
+++ server/src/filter/not-found-exception.filter.ts
@@ -1,7 +1,8 @@
 import { ArgumentsHost, Catch, ExceptionFilter, NotFoundException } from '@nestjs/common';
 import { Request, Response } from 'express';
+import fs from 'fs';
 import path from 'path';
-import { API_PREFIX } from '../main';
+import { SettingsService } from '../module/settings/settings.service';
 
 const ALLOWED_EXTENSIONS = [
   '.js',
@@ -30,7 +31,10 @@ export class NotFoundExceptionFilter implements ExceptionFilter {
   /**
    * @param staticPath Path to the folder which holds the static assets.
    */
-  constructor(private readonly staticPath: string = '/static') {}
+  constructor(
+    private readonly settings: SettingsService,
+    private readonly staticPath: string = '/static'
+  ) {}
 
   /**
    * Handles the catched exception according to the class documentation.
@@ -44,12 +48,39 @@ export class NotFoundExceptionFilter implements ExceptionFilter {
     const response = ctx.getResponse<Response>();
     const status = exception.getStatus();
 
-    if (url.startsWith(`/${API_PREFIX}`)) {
+    if (url.startsWith(`/${this.settings.getAPIPrefix()}`)) {
       return response.status(status).send(exception.message);
     } else if (ALLOWED_EXTENSIONS.filter((ext) => url.indexOf(ext) > 0).length > 0) {
       this.sendFile(url, response);
     } else {
-      this.sendFile('index.html', response);
+      this.sendIndexFile(response);
+    }
+  }
+
+  /**
+   * Send an adjusted index file to the requested user.
+   *
+   * The index file will get adjusted by adding some global variables needed by the client (ie 'ROUTE_PREFIX' to let the client know that the app is hosted on a prefixed path).
+   *
+   * @param response Response to send the index file to.
+   */
+  private sendIndexFile(response: Response): void {
+    const index: string = fs.readFileSync(this.resolvePath('index.html')).toString();
+    const replaced = index.replace(
+      /<!--{{global}}-->/g,
+      `<script>\n\tconst ROUTE_PREFIX = ${this.getStringForPrefix()};\n</script>`
+    );
+
+    response.send(replaced);
+  }
+
+  private getStringForPrefix(): string {
+    const prefix = this.settings.getPathPrefix();
+
+    if (!!prefix) {
+      return `"${prefix}"`;
+    } else {
+      return 'null';
     }
   }
 
