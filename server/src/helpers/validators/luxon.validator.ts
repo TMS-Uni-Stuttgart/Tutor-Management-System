028cf601d596fe0d5ba0c0af702b3dea3a66e572
Dudrie <Dudrie@gmx.de>
2020-05-29 17:33:33 +0200

    Add validator for luxon Interval objects.

--- server/src/helpers/validators/luxon.validator.ts
+++ server/src/helpers/validators/luxon.validator.ts
@@ -1,5 +1,5 @@
 import { registerDecorator, ValidationOptions } from 'class-validator';
-import { DateTime } from 'luxon';
+import { DateTime, Interval } from 'luxon';
 
 /**
  * Validates the property to match the luxon format of a date in the ISO format.
@@ -24,6 +24,35 @@ export function IsLuxonDateTime(validationOptions?: ValidationOptions) {
           const date = DateTime.fromISO(value);
 
           return date.isValid;
+        },
+      },
+    });
+  };
+}
+
+/**
+ * Validates the property to match the luxon format of an interval in the ISO format.
+ *
+ * @param validationOptions Options passed to the class-validator.
+ */
+export function IsLuxonInterval(validationOptions?: ValidationOptions) {
+  return function (object: object, propertyName: string) {
+    const message: any = {
+      message: validationOptions?.each
+        ? `each interval in ${propertyName} must be in a valid ISO format`
+        : `${propertyName} must be in the ISO format`,
+    };
+
+    registerDecorator({
+      name: 'isLuxonInterval',
+      target: object.constructor,
+      propertyName,
+      options: { ...message, ...validationOptions },
+      validator: {
+        validate(value: any): boolean {
+          const interval = Interval.fromISO(value);
+
+          return interval.isValid && !interval.invalidReason;
         },
       },
     });
