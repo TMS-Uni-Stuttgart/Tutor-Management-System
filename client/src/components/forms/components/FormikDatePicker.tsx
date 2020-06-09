06bc259e6ddbbb68c24b618a613c1436cdac550b
Dudrie <Dudrie@gmx.de>
2020-06-09 19:31:37 +0200

    Add error to FormikDatePicker if entered day is invalid.

--- client/src/components/forms/components/FormikDatePicker.tsx
+++ client/src/components/forms/components/FormikDatePicker.tsx
@@ -1,5 +1,5 @@
 import { Field, FieldProps } from 'formik';
-import React from 'react';
+import React, { useState } from 'react';
 import CustomDatePicker, { CustomDatePickerProps } from '../../date-picker/DatePicker';
 
 interface Props {
@@ -9,6 +9,8 @@ interface Props {
 type PropType = Props & Omit<CustomDatePickerProps, keyof FieldProps['field']>;
 
 function FormikDatePicker({ name, className, ...other }: PropType): JSX.Element {
+  const [innerError, setInnerError] = useState<string>();
+
   return (
     <Field name={name}>
       {({ field, form, meta: { touched, error } }: FieldProps) => (
@@ -17,11 +19,14 @@ function FormikDatePicker({ name, className, ...other }: PropType): JSX.Element 
           {...other}
           onChange={(date) => {
             if (date && date.isValid) {
+              setInnerError(undefined);
               form.setFieldValue(field.name, date, true);
+            } else {
+              setInnerError('Ungültiges Datum');
             }
           }}
-          helperText={!!touched && error}
-          error={touched && !!error}
+          helperText={!!touched && (error || innerError)}
+          error={touched && (!!error || !!innerError)}
           className={className}
           inputVariant='outlined'
         />
