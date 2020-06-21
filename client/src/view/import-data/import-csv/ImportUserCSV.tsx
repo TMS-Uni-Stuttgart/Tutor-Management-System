b52a23303387aa3a76d35ab121b124d37e662c1a
Dudrie <Dudrie@gmx.de>
2020-06-21 19:18:09 +0200

    Add first iteration of ImportUserCSV component.

--- client/src/view/import-data/import-csv/ImportUserCSV.tsx
+++ client/src/view/import-data/import-csv/ImportUserCSV.tsx
@@ -1,7 +1,31 @@
+import { Box, TextField, Typography } from '@material-ui/core';
 import React from 'react';
 
 function ImportUserCSV(): JSX.Element {
-  return <div>IMPORT_CSV</div>;
+  return (
+    <Box display='flex' flexDirection='column' width='100%'>
+      <Box display='flex' marginBottom={2} alignItems='center'>
+        <Typography variant='h4'>CSV importieren</Typography>
+
+        {/* TODO: Oder Dropdown-Box? */}
+        <TextField
+          variant='outlined'
+          label='Seperator'
+          placeholder='Auto'
+          style={{ marginLeft: 'auto' }}
+        />
+      </Box>
+
+      <TextField
+        variant='outlined'
+        label='CSV Daten'
+        placeholder='CSV hier einfügen'
+        inputProps={{ rowsMin: 8 }}
+        fullWidth
+        multiline
+      />
+    </Box>
+  );
 }
 
 export default ImportUserCSV;
