6de5c7abe62b3f35a6ed45befe02b765feb7699c
Dudrie <Dudrie@gmx.de>
2020-06-24 21:50:06 +0200

    Add upload button to ImportUserCSV.
    This lets the user choose a .csv file. If "valid" (against a simple MIME type check) the content of the file will be set as csvInput - if later will be overridden by this action the user has to confirm the override.

--- client/src/view/import-data/import-csv/ImportUserCSV.tsx
+++ client/src/view/import-data/import-csv/ImportUserCSV.tsx
@@ -1,21 +1,37 @@
-import { Box, TextField, Typography } from '@material-ui/core';
+import { Box, Button, Divider, TextField, Typography } from '@material-ui/core';
+import { createStyles, makeStyles } from '@material-ui/core/styles';
+import { Upload as UploadIcon } from 'mdi-material-ui';
 import { SnackbarKey, useSnackbar } from 'notistack';
 import React, { useCallback, useEffect, useState } from 'react';
+import LoadingModal from '../../../components/loading/LoadingModal';
 import SnackbarWithList from '../../../components/SnackbarWithList';
 import {
   NextStepCallback,
   useStepper,
 } from '../../../components/stepper-with-buttons/context/StepperContext';
+import { useDialog } from '../../../hooks/DialogService';
 import { getParsedCSV } from '../../../hooks/fetching/CSV';
 import { useImportDataContext } from '../ImportUsers.context';
 
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    divider: { flex: 1, margin: theme.spacing(0, 2) },
+    uploadLabel: { flex: 1 },
+    uploadButton: { width: '100%' },
+  })
+);
+
 function ImportUserCSV(): JSX.Element {
+  const classes = useStyles();
+
   const { setNextCallback, removeNextCallback, setNextDisabled } = useStepper();
   const { setData, csvFormData, setCSVFormData } = useImportDataContext();
   const { enqueueSnackbar } = useSnackbar();
+  const { showConfirmationDialog } = useDialog();
 
   const [csvInput, setCSVInput] = useState(csvFormData?.csvInput ?? '');
   const [seperator, setSeparator] = useState(csvFormData?.seperator ?? '');
+  const [isLoadingCSV, setLoadingCSV] = useState(false);
 
   const handleSubmit: NextStepCallback = useCallback(async () => {
     try {
@@ -60,6 +76,43 @@ function ImportUserCSV(): JSX.Element {
     return removeNextCallback;
   }, [setNextCallback, removeNextCallback, handleSubmit]);
 
+  const handleFileUpload = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    // We need the target after async code to reset the text field.
+    e.persist();
+    const file = e.target.files?.[0];
+
+    if (!file) {
+      return;
+    }
+
+    if (file.type !== 'application/vnd.ms-excel') {
+      enqueueSnackbar('Ausgewählte Datei ist keine CSV-Datei.', { variant: 'error' });
+      return;
+    }
+
+    setLoadingCSV(true);
+    if (!!csvInput) {
+      const overrideExisting = await showConfirmationDialog({
+        title: 'Überschreiben?',
+        content:
+          'Es sind noch Daten im Textfeld vorhanden. Sollen diese wirklich überschrieben werden?',
+        acceptProps: { label: 'Überschreiben', deleteButton: true },
+        cancelProps: { label: 'Nicht überschreiben' },
+      });
+
+      if (!overrideExisting) {
+        setLoadingCSV(false);
+        return;
+      }
+    }
+    const content: string = await file.text();
+
+    // Reset the file so the user could select the same file twice.
+    e.target.value = '';
+    setCSVInput(content);
+    setLoadingCSV(false);
+  };
+
   return (
     <Box display='flex' flexDirection='column' width='100%'>
       <Box display='flex' marginBottom={2} alignItems='center'>
@@ -78,14 +131,42 @@ function ImportUserCSV(): JSX.Element {
 
       <TextField
         variant='outlined'
-        label='CSV Daten'
-        placeholder='CSV hier einfügen'
+        label='CSV Daten einfügen'
         inputProps={{ rowsMin: 8 }}
         value={csvInput}
         onChange={(e) => setCSVInput(e.target.value)}
         fullWidth
         multiline
       />
+
+      <Box display='flex' alignItems='center' marginY={2}>
+        <Divider className={classes.divider} />
+        <Typography>ODER</Typography>
+        <Divider className={classes.divider} />
+      </Box>
+
+      <Box display='flex'>
+        <input
+          accept='.csv'
+          style={{ display: 'none' }}
+          id='icon-button-file'
+          type='file'
+          onChange={handleFileUpload}
+        />
+        <label htmlFor='icon-button-file' className={classes.uploadLabel}>
+          <Button
+            variant='outlined'
+            disableElevation
+            className={classes.uploadButton}
+            component='span'
+            startIcon={<UploadIcon />}
+          >
+            CSV-Datei hochladen
+          </Button>
+        </label>
+      </Box>
+
+      <LoadingModal modalText='Importiere CSV...' open={isLoadingCSV} />
     </Box>
   );
 }
