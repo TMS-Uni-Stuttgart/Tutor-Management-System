f1b93309a00c3926bbc1536ddba2f1f657150865
Dudrie <Dudrie@gmx.de>
2020-07-03 07:01:09 +0200

    Change TutorialRailItem to use the new router logic.

--- client/src/components/navigation-rail/components/TutorialRailItem.tsx
+++ client/src/components/navigation-rail/components/TutorialRailItem.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { LoggedInUser } from '../../../model/LoggedInUser';
 import { TutorialRelatedDrawerRoute } from '../../../routes/newVersion/Routing.types';
-import { getTutorialRelatedPath } from '../../../routes/Routing.helpers';
 import RailItem from './RailItem';
 import { getSubItems } from './TutorialRailItem.helpers';
 
@@ -19,7 +18,12 @@ function TutorialRailItem({ route, userData }: Props): JSX.Element | null {
 
   if (subItems.length === 1) {
     return (
-      <RailItem key={route.path} path={subItems[0].subPath} text={route.title} icon={route.icon} />
+      <RailItem
+        key={route.template}
+        path={subItems[0].subPath}
+        text={route.title}
+        icon={route.icon}
+      />
     );
   }
 
@@ -27,8 +31,8 @@ function TutorialRailItem({ route, userData }: Props): JSX.Element | null {
     userData.tutorials[0] || userData.tutorialsToCorrect[0] || userData.substituteTutorials[0];
   return (
     <RailItem
-      key={route.path}
-      path={getTutorialRelatedPath(route, tutorial.id)}
+      key={route.template}
+      path={route.create({ tutorialId: tutorial.id })}
       text={route.title}
       icon={route.icon}
       subItems={getSubItems(route, userData)}
