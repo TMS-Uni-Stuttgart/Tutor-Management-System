d054c30615ab3f95a61a6cda62190274c6a9378f
Dudrie <Dudrie@gmx.de>
2020-07-04 21:43:52 +0200

    Fix several issues with the MemoryRouter in TutorialInternalsManagement.

--- client/src/view/tutorial-internals-management/TutorialInternalsManagement.tsx
+++ client/src/view/tutorial-internals-management/TutorialInternalsManagement.tsx
@@ -1,12 +1,13 @@
-import { Box, Divider, Typography } from '@material-ui/core';
+import { Box, Button, Divider, Typography } from '@material-ui/core';
 import { createStyles, makeStyles } from '@material-ui/core/styles';
-import React, { useMemo, useState } from 'react';
+import { ChevronLeft as BackIcon } from 'mdi-material-ui';
+import React, { useCallback, useMemo, useState } from 'react';
 import { MemoryRouter, useHistory } from 'react-router-dom';
-import BackButton from '../../components/BackButton';
 import CustomSelect, { OnChangeHandler } from '../../components/CustomSelect';
 import Placeholder from '../../components/Placeholder';
 import { useTutorialFromPath } from '../../hooks/useTutorialFromPath';
-import { ROUTES } from '../../routes/Routing.routes';
+import { Tutorial } from '../../model/Tutorial';
+import { ROUTES, TUTORIAL_ROUTES } from '../../routes/Routing.routes';
 import { TutorialRelatedDrawerRoute } from '../../routes/Routing.types';
 import Routes from './components/Routes';
 
@@ -19,34 +20,35 @@ const useStyles = makeStyles((theme) =>
     dropdown: {
       flex: 1,
     },
+    backIcon: {
+      marginLeft: theme.spacing(-0.5),
+      marginRight: theme.spacing(0.5),
+    },
+    backButton: {
+      width: 'max-content',
+    },
   })
 );
 
 interface Props {
   routes: TutorialRelatedDrawerRoute[];
+  tutorial: Tutorial | undefined;
+  isLoading: boolean;
+  error?: string;
+  onBackClick: () => void;
 }
 
 function getAllRelatedRoutes(): TutorialRelatedDrawerRoute[] {
-  const routes: TutorialRelatedDrawerRoute[] = [];
-
-  for (const route of Object.values(ROUTES)) {
-    if (route.isTutorialRelatedDrawerRoute()) {
-      routes.push(route);
-    }
-  }
-
-  return routes;
+  return [...Object.values(TUTORIAL_ROUTES)] as any;
 }
 
-function Content({ routes }: Props): JSX.Element {
+function Content({ routes, tutorial, isLoading, error, onBackClick }: Props): JSX.Element {
   const classes = useStyles();
 
   const history = useHistory();
-  const { tutorial, isLoading, error } = useTutorialFromPath();
-
   const [value, setValue] = useState('');
 
-  const items = useMemo(() => routes.filter((r) => r.isInDrawer), [routes]);
+  const items = useMemo(() => routes.filter((r) => r.isInDrawer && r.isTutorialRelated), [routes]);
 
   const onPathSelect: OnChangeHandler = (e) => {
     if (typeof e.target.value !== 'string') {
@@ -71,7 +73,10 @@ function Content({ routes }: Props): JSX.Element {
   return (
     <Box display='flex' flexDirection='column'>
       <Box marginBottom={2} display='flex' alignItems='center'>
-        <BackButton to={ROUTES.MANAGE_TUTORIALS.create({})} />
+        <Button variant='outlined' className={classes.backButton} onClick={onBackClick}>
+          <BackIcon className={classes.backIcon} />
+          Zurück zur Übersicht
+        </Button>
 
         <Typography className={classes.header} variant='h5'>
           {!!tutorial ? `Verwalte ${tutorial.toDisplayString()}` : 'Kein Tutorium.'}
@@ -105,11 +110,23 @@ function Content({ routes }: Props): JSX.Element {
 }
 
 function TutorialInternalsManagement(): JSX.Element {
+  const history = useHistory();
+  const { tutorial, isLoading, error } = useTutorialFromPath();
+
   const routes: TutorialRelatedDrawerRoute[] = useMemo(() => getAllRelatedRoutes(), []);
+  const handleBackClick = useCallback(() => {
+    history.push({ pathname: ROUTES.MANAGE_TUTORIALS.create({}) });
+  }, [history]);
 
   return (
     <MemoryRouter>
-      <Content routes={routes} />
+      <Content
+        routes={routes}
+        onBackClick={handleBackClick}
+        tutorial={tutorial}
+        isLoading={isLoading}
+        error={error}
+      />
     </MemoryRouter>
   );
 }
