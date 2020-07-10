4d500553c487a127feadc66156b0b95313866c87
Dudrie <Dudrie@gmx.de>
2020-07-10 17:52:26 +0200

    Add unified function to throw an 'Uninitialized context' error.

---
+++ client/src/util/throwFunctions.ts
@@ -0,0 +1,11 @@
+/**
+ * Returns a function that throws an Error indicating that the corresponding context is not initialized.
+ *
+ * @param contextName Name of the context.
+ * @returns Function that throws an `Error` (see above).
+ */
+export function notInitializied(contextName: string) {
+  return (): never => {
+    throw new Error(`Context '${contextName}' not initialised.`);
+  };
+}
