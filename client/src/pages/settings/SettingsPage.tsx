ebac56b171880b091ec995285a6eda5040fb6a5a
Dudrie <Dudrie@gmx.de>
2020-07-30 17:48:31 +0200

    Add e-mail settings to the settings page.

--- client/src/pages/settings/SettingsPage.tsx
+++ client/src/pages/settings/SettingsPage.tsx
@@ -1,17 +1,20 @@
-import { Box, Divider, Typography } from '@material-ui/core';
+import { Box, Button, Divider, Typography } from '@material-ui/core';
 import { createStyles, makeStyles } from '@material-ui/core/styles';
-import { Formik } from 'formik';
+import { Formik, useFormikContext } from 'formik';
 import React, { useCallback, useMemo } from 'react';
 import { IClientSettings } from 'shared/model/Settings';
 import * as Yup from 'yup';
 import FormikCheckbox from '../../components/forms/components/FormikCheckbox';
+import FormikDebugDisplay from '../../components/forms/components/FormikDebugDisplay';
 import FormikTextField from '../../components/forms/components/FormikTextField';
 import SubmitButton from '../../components/loading/SubmitButton';
 import Placeholder from '../../components/Placeholder';
+import { useDialog } from '../../hooks/DialogService';
 import { setSettings } from '../../hooks/fetching/Settings';
 import { useCustomSnackbar } from '../../hooks/snackbar/useCustomSnackbar';
 import { useSettings } from '../../hooks/useSettings';
 import { FormikSubmitCallback } from '../../types';
+import EMailSettings from './components/EMailSettings';
 
 const useStyles = makeStyles((theme) =>
   createStyles({
@@ -21,40 +24,205 @@ const useStyles = makeStyles((theme) =>
   })
 );
 
-const validationSchema = Yup.object<FormState>().shape({
+const validationSchema = Yup.object().shape({
   canTutorExcuseStudents: Yup.boolean().required('Benötigt'),
   defaultTeamSize: Yup.number()
     .integer('Muss eine ganze Zahl sein.')
     .min(1, 'Muss mindestens 1 sein.')
     .required('Benötigt'),
+  mailingConfig: Yup.lazy((value: { enabled?: boolean } | undefined | null) => {
+    if (value?.enabled) {
+      return Yup.object().shape({
+        enabled: Yup.boolean().defined(),
+        from: Yup.string()
+          .required('Benötigt')
+          .test({
+            name: 'isValidFrom',
+            message: 'Muss eine kommaseparierte Liste mit "{email}" oder "{name} <{email}>" sein',
+            test: (value) => {
+              if (typeof value !== 'string') {
+                return false;
+              }
+
+              const regexMail = /[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*/;
+              const regexName = /([\p{L}\p{N}",*-]|[^\S\r\n])+/;
+              const regex = new RegExp(
+                `^(${regexMail.source})|(${regexName.source} <${regexMail.source}>)$`,
+                'u'
+              );
+              const mails = value.split(',').map((m) => m.trim());
+
+              for (const mail of mails) {
+                if (!regex.test(mail)) {
+                  return false;
+                }
+              }
+
+              return true;
+            },
+          }),
+        host: Yup.string().required('Benötigt'),
+        port: Yup.number()
+          .positive('Muss eine positive Zahl sein')
+          .integer('Muss eine ganze Zahl sein')
+          .required('Benötigt'),
+        auth: Yup.object().shape({
+          user: Yup.string().required('Benötigt.'),
+          pass: Yup.string().required('Benötigt.'),
+        }),
+      });
+    } else {
+      return Yup.mixed();
+    }
+  }),
 });
 
 interface FormState {
   defaultTeamSize: string;
   canTutorExcuseStudents: boolean;
+  mailingConfig: {
+    enabled: boolean;
+    from: string;
+    host: string;
+    port: string;
+    auth: { user: string; pass: string };
+  };
 }
 
+function getInitialValues(settings: IClientSettings): FormState {
+  const { mailingConfig, canTutorExcuseStudents, defaultTeamSize } = settings;
+  return {
+    canTutorExcuseStudents: canTutorExcuseStudents,
+    defaultTeamSize: `${defaultTeamSize}`,
+    mailingConfig: {
+      enabled: !!mailingConfig,
+      from: mailingConfig?.from ?? '',
+      host: mailingConfig?.host ?? '',
+      port: `${mailingConfig?.port ?? ''}`,
+      auth: { user: mailingConfig?.auth.user ?? '', pass: mailingConfig?.auth.pass ?? '' },
+    },
+  };
+}
+
+function convertFormStateToDTO(values: FormState): IClientSettings {
+  const dto: IClientSettings = {
+    canTutorExcuseStudents: values.canTutorExcuseStudents,
+    defaultTeamSize: Number.parseInt(values.defaultTeamSize),
+  };
+
+  if (values.mailingConfig.enabled) {
+    const { from, host, port, auth } = values.mailingConfig;
+    dto.mailingConfig = {
+      from,
+      host,
+      port: Number.parseInt(port),
+      auth: { user: auth.user, pass: auth.pass },
+    };
+  }
+
+  return dto;
+}
+
+// TODO: Extract me?!
 function GridDivider(): JSX.Element {
   return <Divider style={{ gridColumn: '1 / -1' }} />;
 }
 
-function SettingsPage(): JSX.Element {
+function SettingsPageForm(): JSX.Element {
   const classes = useStyles();
+  const { showConfirmationDialog } = useDialog();
+  const { handleSubmit, isSubmitting, dirty, isValid, resetForm } = useFormikContext<FormState>();
+
+  const handleFormReset = useCallback(async () => {
+    const result = await showConfirmationDialog({
+      title: 'Einstellungen zurücksetzen?',
+      content:
+        'Sollen die Einstellungen wirklich zurückgesetzt werden? Dies kann nicht rückgängig gemacht werden!',
+      acceptProps: { label: 'Zurücksetzen', deleteButton: true },
+      cancelProps: { label: 'Abbrechen' },
+    });
+
+    if (result) {
+      resetForm();
+    }
+  }, [showConfirmationDialog, resetForm]);
+
+  return (
+    <form onSubmit={handleSubmit} className={classes.form}>
+      <Box display='flex' alignItems='center' marginBottom={3}>
+        <SubmitButton
+          variant='contained'
+          color='primary'
+          isSubmitting={isSubmitting}
+          disabled={!dirty || !isValid}
+        >
+          Einstellungen speichern
+        </SubmitButton>
+
+        {dirty && (
+          <Typography color='error' className={classes.unsavedChangesLabel}>
+            Es gibt ungespeicherte Änderungen.
+          </Typography>
+        )}
+
+        <Button
+          onClick={handleFormReset}
+          style={{ marginLeft: 'auto' }}
+          variant='outlined'
+          disabled={!dirty}
+        >
+          Formular zurücksetzen
+        </Button>
+      </Box>
+
+      <Box
+        flex={1}
+        display='grid'
+        gridTemplateColumns='fit-content(50%) 1fr'
+        gridAutoRows='min-content'
+        alignItems='baseline'
+        gridColumnGap={24}
+        gridRowGap={12}
+        height='auto'
+        maxHeight='100%'
+        style={{ overflowY: 'auto' }}
+      >
+        <Typography style={{ fontSize: '1.1rem' }}>Standardteamgröße</Typography>
+        <FormikTextField
+          label='Standardteamgröße'
+          name='defaultTeamSize'
+          type='number'
+          className={classes.input}
+          required
+        />
+
+        <GridDivider />
+
+        <Typography style={{ fontSize: '1.1rem' }}>Anwesenheiten</Typography>
+        <FormikCheckbox
+          label='Tutoren/innen dürfen Studierende entschuldigen'
+          name='canTutorExcuseStudents'
+        />
+
+        <GridDivider />
+
+        <Typography style={{ fontSize: '1.1rem' }}>E-Maileinstellungen</Typography>
+        <EMailSettings />
+      </Box>
+
+      <FormikDebugDisplay showErrors />
+    </form>
+  );
+}
+
+function SettingsPage(): JSX.Element {
   const { isLoadingSettings, settings, updateSettings } = useSettings();
   const { promiseWithSnackbar } = useCustomSnackbar();
 
-  const initialValues: FormState = useMemo(() => {
-    return {
-      canTutorExcuseStudents: settings.canTutorExcuseStudents,
-      defaultTeamSize: `${settings.defaultTeamSize}`,
-    };
-  }, [settings]);
+  const initialValues: FormState = useMemo(() => getInitialValues(settings), [settings]);
   const onSubmit: FormikSubmitCallback<FormState> = useCallback(
     async (values) => {
-      const dto: IClientSettings = {
-        canTutorExcuseStudents: values.canTutorExcuseStudents,
-        defaultTeamSize: Number.parseInt(values.defaultTeamSize),
-      };
+      const dto: IClientSettings = convertFormStateToDTO(values);
 
       await promiseWithSnackbar({
         promiseFunction: setSettings,
@@ -76,60 +244,7 @@ function SettingsPage(): JSX.Element {
       loading={isLoadingSettings}
     >
       <Formik initialValues={initialValues} onSubmit={onSubmit} validationSchema={validationSchema}>
-        {({ handleSubmit, isValid, dirty, isSubmitting }) => (
-          <form onSubmit={handleSubmit} className={classes.form}>
-            <Box display='flex' alignItems='center' marginBottom={3}>
-              <SubmitButton
-                variant='contained'
-                color='primary'
-                isSubmitting={isSubmitting}
-                disabled={!dirty || !isValid}
-              >
-                Einstellungen speichern
-              </SubmitButton>
-
-              {dirty && (
-                <Typography color='error' className={classes.unsavedChangesLabel}>
-                  Es gibt ungespeicherte Änderungen.
-                </Typography>
-              )}
-            </Box>
-
-            <Box
-              flex={1}
-              display='grid'
-              gridTemplateColumns='fit-content(50%) 1fr'
-              gridAutoRows='min-content'
-              alignItems='center'
-              gridColumnGap={24}
-              gridRowGap={12}
-              height='auto'
-              maxHeight='100%'
-              style={{ overflowY: 'auto' }}
-            >
-              <Typography style={{ fontSize: '1.1rem' }}>Standardteamgröße</Typography>
-              <FormikTextField
-                label='Standardteamgröße'
-                name='defaultTeamSize'
-                type='number'
-                className={classes.input}
-              />
-
-              <GridDivider />
-
-              <Typography style={{ fontSize: '1.1rem' }}>Anwesenheiten</Typography>
-              <FormikCheckbox
-                label='Tutoren/innen dürfen Studierende entschuldigen'
-                name='canTutorExcuseStudents'
-              />
-
-              <GridDivider />
-
-              <Typography style={{ fontSize: '1.1rem' }}>E-Maileinstellungen</Typography>
-              <Typography>🛠 Work in progress</Typography>
-            </Box>
-          </form>
-        )}
+        <SettingsPageForm />
       </Formik>
     </Placeholder>
   );
---
+++ client/src/pages/settings/components/EMailSettings.tsx
@@ -0,0 +1,70 @@
+import { Box } from '@material-ui/core';
+import { createStyles, makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
+import { useField } from 'formik';
+import React from 'react';
+import FormikCheckbox from '../../../components/forms/components/FormikCheckbox';
+import FormikPasswordField from '../../../components/forms/components/FormikPasswordField';
+import FormikTextField from '../../../components/forms/components/FormikTextField';
+
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    input: { margin: theme.spacing(1, 0) },
+    portInput: { marginLeft: theme.spacing(1) },
+  })
+);
+
+function EMailSettings(): JSX.Element {
+  const classes = useStyles();
+  const [, { value: isEnabled }] = useField<boolean>('mailingConfig.enabled');
+
+  return (
+    <Box display='grid' gridTemplateColumns='1fr'>
+      <FormikCheckbox label='E-Mails aktivieren' name='mailingConfig.enabled' />
+
+      <Box display='flex'>
+        <FormikTextField
+          label='Mail-Host'
+          name='mailingConfig.host'
+          className={classes.input}
+          disabled={!isEnabled}
+          required={isEnabled}
+        />
+        <FormikTextField
+          label='Mail-Port'
+          name='mailingConfig.port'
+          type='number'
+          className={clsx(classes.input, classes.portInput)}
+          disabled={!isEnabled}
+          required={isEnabled}
+        />
+      </Box>
+
+      <FormikTextField
+        label='Absender'
+        name='mailingConfig.from'
+        className={classes.input}
+        disabled={!isEnabled}
+        required={isEnabled}
+      />
+
+      <FormikTextField
+        label='Nutzername'
+        name='mailingConfig.auth.user'
+        className={classes.input}
+        disabled={!isEnabled}
+        required={isEnabled}
+      />
+
+      <FormikPasswordField
+        label='Passwort'
+        name='mailingConfig.auth.pass'
+        className={classes.input}
+        disabled={!isEnabled}
+        required={isEnabled}
+      />
+    </Box>
+  );
+}
+
+export default EMailSettings;
