1c2f85aafa8a23527c82202eec95b97e1f01034e
Dudrie <Dudrie@gmx.de>
2020-08-20 16:24:01 +0200

    Add determining the next shortTestNo in AdjustGeneratedShortTest.

--- client/src/pages/import-short-tests/components/AdjustGeneratedShortTest.tsx
+++ client/src/pages/import-short-tests/components/AdjustGeneratedShortTest.tsx
@@ -4,8 +4,11 @@ import { useFormikContext } from 'formik';
 import React, { useCallback, useEffect, useMemo } from 'react';
 import ShortTestForm, { ShortTestFormState } from '../../../components/forms/ShortTestForm';
 import { useMapColumnsHelpers } from '../../../components/import-csv/hooks/useMapColumnsHelpers';
+import Placeholder from '../../../components/Placeholder';
 import { useStepper } from '../../../components/stepper-with-buttons/context/StepperContext';
+import { getAllShortTests } from '../../../hooks/fetching/ShortTests';
 import { useCustomSnackbar } from '../../../hooks/snackbar/useCustomSnackbar';
+import { useFetchState } from '../../../hooks/useFetchState';
 import { FormikSubmitCallback } from '../../../types';
 import { ShortTestColumns } from '../ImportShortTests';
 
@@ -46,11 +49,20 @@ function HookUpStepper(): null {
 function AdjustGeneratedShortTest(): JSX.Element {
   const classes = useStyles();
   const { data, mappedColumns } = useMapColumnsHelpers<ShortTestColumns>();
+  const { isLoading, value: shortTests } = useFetchState({
+    fetchFunction: getAllShortTests,
+    immediate: true,
+    params: [],
+  });
   const { enqueueSnackbar } = useCustomSnackbar();
 
-  const initialValues: ShortTestFormState = useMemo(
-    () => ({
-      shortTestNo: '',
+  const initialValues: ShortTestFormState = useMemo(() => {
+    const nextShortTestNo: number | undefined = shortTests
+      ? shortTests[shortTests.length - 1].shortTestNo + 1
+      : undefined;
+
+    return {
+      shortTestNo: nextShortTestNo?.toString(10) ?? '',
       percentageNeeded: 0.5,
       exercises: [
         {
@@ -60,9 +72,8 @@ function AdjustGeneratedShortTest(): JSX.Element {
           subexercises: [],
         },
       ],
-    }),
-    [data.rows, mappedColumns.testMaximumPoints]
-  );
+    };
+  }, [shortTests, data.rows, mappedColumns.testMaximumPoints]);
 
   const handleSubmit: FormikSubmitCallback<ShortTestFormState> = useCallback(async (values) => {
     return true;
@@ -72,14 +83,20 @@ function AdjustGeneratedShortTest(): JSX.Element {
     <Box display='flex' flexDirection='column' flex={1}>
       <Typography variant='h4'>Importierten Kurztest anpassen</Typography>
 
-      <ShortTestForm
-        onSubmit={handleSubmit}
-        initialValues={initialValues}
-        className={classes.form}
-        hideSaveButton
+      <Placeholder
+        placeholderText='Keine Anpassung möglich'
+        showPlaceholder={false}
+        loading={isLoading}
       >
-        <HookUpStepper />
-      </ShortTestForm>
+        <ShortTestForm
+          onSubmit={handleSubmit}
+          initialValues={initialValues}
+          className={classes.form}
+          hideSaveButton
+        >
+          <HookUpStepper />
+        </ShortTestForm>
+      </Placeholder>
     </Box>
   );
 }
