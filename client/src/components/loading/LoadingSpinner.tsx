b03602921b6be774f1814862357e4caf5984a0c3
Dudrie <Dudrie@gmx.de>
2020-08-27 17:02:01 +0200

    Add props to LoadingSpinner.

--- client/src/components/loading/LoadingSpinner.tsx
+++ client/src/components/loading/LoadingSpinner.tsx
@@ -1,5 +1,6 @@
 import { CircularProgress, Theme, Typography } from '@material-ui/core';
 import { createStyles, makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
 import React from 'react';
 
 const useStyles = makeStyles((theme: Theme) =>
@@ -8,9 +9,11 @@ const useStyles = makeStyles((theme: Theme) =>
       display: 'flex',
       flexDirection: 'column',
       width: '100%',
-      height: '100%',
       alignItems: 'center',
       justifyContent: 'center',
+    },
+    fullHeight: {
+      height: '100%',
     },
     spinner: {
       marginBottom: theme.spacing(3),
@@ -18,15 +21,20 @@ const useStyles = makeStyles((theme: Theme) =>
   })
 );
 
-function LoadingSpinner(): JSX.Element {
+export interface LoadingSpinnerProps {
+  shrinkBox?: boolean;
+  text?: string;
+}
+
+function LoadingSpinner({ shrinkBox, text }: LoadingSpinnerProps): JSX.Element {
   const classes = useStyles();
 
   return (
-    <div className={classes.spinnerBox}>
+    <div className={clsx(classes.spinnerBox, !shrinkBox && classes.fullHeight)}>
       <CircularProgress className={classes.spinner} color='primary' />
 
       <Typography variant='h5' color='primary'>
-        Lade Daten...
+        {`${text || 'Lade Daten'}...`}
       </Typography>
     </div>
   );
