2970681aac2a549f94e8348eb0071e13ee08f131
Dudrie <Dudrie@gmx.de>
2020-10-09 14:06:50 +0200

    Fix correctors lacking permission to enter gradings for students.

--- server/src/guards/decorators/roles.decorator.ts
+++ server/src/guards/decorators/roles.decorator.ts
@@ -3,5 +3,12 @@ import { Role } from '../../shared/model/Role';
 
 export const ROLE_METADATA_KEY = 'roles';
 
+/**
+ * Configures the roles for guards.
+ *
+ * Can be used to determine which roles have access to an endpoint for example.
+ *
+ * @param roles Roles which can be read by the guard.
+ */
 export const Roles = (...roles: Role[]): CustomDecorator<string> =>
   SetMetadata(ROLE_METADATA_KEY, roles);
