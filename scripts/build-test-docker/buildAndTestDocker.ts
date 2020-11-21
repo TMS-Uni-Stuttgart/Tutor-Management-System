cdc57fe63ad3ff278e0503986ed6700c650818bc
Dudrie <Dudrie@gmx.de>
2020-11-21 21:11:05 +0100

    Rename folder dev-tools/ to scripts/

--- scripts/build-test-docker/buildAndTestDocker.ts
+++ scripts/build-test-docker/buildAndTestDocker.ts
@@ -1,28 +1,12 @@
 /* eslint-disable no-console */
 import axios from 'axios';
 import chalk from 'chalk';
-import {
-  ChildProcessWithoutNullStreams,
-  spawn,
-  SpawnOptionsWithoutStdio,
-  spawnSync,
-} from 'child_process';
-import os from 'os';
+import { spawn, SpawnOptionsWithoutStdio, spawnSync } from 'child_process';
+import { addConsoleToProcess, getCommand, stopOnError } from '../util/cmdHelpers';
 import { login } from '../util/login';
 
-function addConsoleToProcess(childProcess: ChildProcessWithoutNullStreams) {
-  childProcess.stdout.pipe(process.stdout);
-  childProcess.stderr.pipe(process.stderr);
-}
-
-function stopOnError(msg: string): never {
-  console.error(chalk.red(msg));
-  process.exit(1);
-}
-
-function getCommand(command: string): string {
-  return os.platform() === 'win32' ? `${command}.cmd` : command;
-}
+const IMAGE_NAME = 'dudrie/tms-test';
+const spawnOptions: SpawnOptionsWithoutStdio = { cwd: './build-test-docker' };
 
 async function wait(ms: number): Promise<void> {
   return new Promise((resolve) => {
@@ -78,8 +62,64 @@ async function tryPdf() {
   }
 }
 
+async function testImage() {
+  let exitCode = 0;
+  try {
+    await tryToConnect();
+
+    console.log(chalk.green('Successfully connected to the running TMS container.'));
+
+    await tryPdf();
+
+    console.log(chalk.green('Successfully generated a PDF.'));
+  } catch (err) {
+    console.error(chalk.red(err));
+    exitCode = 1;
+  } finally {
+    console.log(chalk.blue('Shutting down containers...'));
+
+    const result = spawnSync('docker-compose', ['down', '-v'], {
+      ...spawnOptions,
+      stdio: 'inherit',
+    });
+
+    if (result.error) {
+      stopOnError(result.error.message);
+    }
+
+    console.log(chalk.green('Containers are shut down.'));
+
+    console.log(chalk.blue(`Exiting process (code: ${exitCode})`));
+    process.exitCode = exitCode;
+  }
+}
+
+async function spawnContainer() {
+  console.log(chalk.blue('Starting image in container...'));
+
+  const containerProcess = spawn('docker-compose', ['up'], spawnOptions);
+  addConsoleToProcess(containerProcess);
+
+  containerProcess.on('exit', (code) => {
+    if (code !== 0) {
+      stopOnError(`Could not start docker container with image '${IMAGE_NAME}'.`);
+    }
+  });
+
+  containerProcess.on('message', (msg) => {
+    console.log(msg.toString());
+  });
+
+  containerProcess.on('error', (err) => {
+    stopOnError(`${err.name}: ${err.message}`);
+  });
+
+  await wait(5000);
+
+  testImage();
+}
+
 async function run(): Promise<void> {
-  const IMAGE_NAME = 'dudrie/tms-test';
   spawnSync('clear');
 
   console.log(chalk.blue(`Start building a docker image '${IMAGE_NAME}' ...`));
@@ -101,53 +141,7 @@ async function run(): Promise<void> {
     }
 
     console.log(chalk.green(`Docker image '${IMAGE_NAME}' successfully build.`));
-    console.log(chalk.blue('Starting image in container...'));
-
-    const spawnOptions: SpawnOptionsWithoutStdio = { cwd: './build-test-docker' };
-    const containerProcess = spawn('docker-compose', ['up'], spawnOptions);
-    addConsoleToProcess(containerProcess);
-
-    containerProcess.on('exit', (code) => {
-      if (code !== 0) {
-        stopOnError(`Could not start docker container with image '${IMAGE_NAME}'.`);
-      }
-    });
-
-    containerProcess.on('error', (err) => {
-      stopOnError(`${err.name}: ${err.message}`);
-    });
-
-    await wait(10000);
-
-    let exitCode = 0;
-    try {
-      await tryToConnect();
-
-      console.log(chalk.green('Successfully connected to the running TMS container.'));
-
-      await tryPdf();
-
-      console.log(chalk.green('Successfully generated a PDF.'));
-    } catch (err) {
-      console.error(chalk.red(err));
-      exitCode = 1;
-    } finally {
-      console.log(chalk.blue('Shutting down containers...'));
-
-      const result = spawnSync('docker-compose', ['down', '-v'], {
-        ...spawnOptions,
-        stdio: 'inherit',
-      });
-
-      if (result.error) {
-        stopOnError(result.error.message);
-      }
-
-      console.log(chalk.green('Containers are shut down.'));
-
-      console.log(chalk.blue(`Exiting process (code: ${exitCode})`));
-      process.exit(exitCode);
-    }
+    spawnContainer();
   });
 }
 
