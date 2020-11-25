5e9e32652f5950914e840c6e6b14dbfa0f66ab36
Dudrie <Dudrie@gmx.de>
2020-11-25 19:19:22 +0100

    Change workflow to only deploy if push on main.

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -46,7 +46,7 @@ module.exports = {
     },
     footer: {
       style: 'dark',
-      copyright: `Copyright © ${new Date().getFullYear()}. Built with Docusaurus. (Test deploy)`,
+      copyright: `Copyright © ${new Date().getFullYear()}. Built with Docusaurus.`,
     },
   },
   presets: [
