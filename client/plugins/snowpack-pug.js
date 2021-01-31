a330d1985a1f60dfe41e67bf2ab39d5a5de3cef3
Dudrie <Dudrie@gmx.de>
2021-01-31 19:13:47 +0100

    Migrate to snowpack v3.

--- client/plugins/snowpack-pug.js
+++ client/plugins/snowpack-pug.js
@@ -5,7 +5,6 @@ const fs = require('fs');
 /**
  * @type { import('snowpack').SnowpackPluginFactory<{
  *  file?: string,
- *  excludePugFromBuild?: boolean,
  *  locals?: {
  *    dev?: Record<string, string>,
  *    build?: Record<string, string>,
@@ -17,25 +16,21 @@ module.exports = function (_, pluginOptions) {
     name: 'dudrie/snowpack-pug',
     resolve: {
       input: ['.pug'],
-      output: ['.html', '.pug'],
+      output: ['.html'],
     },
     config(snowpackConfig) {
       const fallback = pluginOptions.file || 'index.pug';
       snowpackConfig.devOptions.fallback = fallback;
     },
-    async load({ filePath, isDev }) {
-      const { locals, excludePugFromBuild } = pluginOptions;
+    load({ filePath, isDev }) {
+      const { locals } = pluginOptions;
       const template = fs.readFileSync(filePath).toString();
+      const render = pug.compile(template);
+      const options = isDev ? locals.dev : locals.build;
 
-      if (isDev) {
-        return { '.html': pug.render(template, { ...locals.dev }) };
-      } else {
-        if (excludePugFromBuild) {
-          return { '.html': pug.render(template, { ...locals.build }) };
-        } else {
-          return { '.pug': template, '.html': pug.render(template, { ...locals.build }) };
-        }
-      }
+      return {
+        '.html': { code: render(options ?? {}) },
+      };
     },
   };
 };
