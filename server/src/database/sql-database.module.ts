b36f161a733eadd463d54d4919cf12f56b4df58d
Dudrie <Dudrie@gmx.de>
2021-04-17 17:10:40 +0200

    Add ts-morph metadata provider.

--- server/src/database/sql-database.module.ts
+++ server/src/database/sql-database.module.ts
@@ -1,14 +1,16 @@
 import { MikroOrmModule } from '@mikro-orm/nestjs';
+import { TsMorphMetadataProvider } from '@mikro-orm/reflection';
 import { Module } from '@nestjs/common';
 
 @Module({
     imports: [
         MikroOrmModule.forRoot({
-            entities: ['./dist/database/entities'],
+            entities: ['./dist/database/entities'], // TODO: Is this path the actual path in the build app?
             entitiesTs: ['./src/database/entities'],
             dbName: 'tms-poc-stuff', // TODO: Make adjustable
             type: 'postgresql',
             baseDir: process.cwd(),
+            metadataProvider: TsMorphMetadataProvider,
             // clientUrl: '', // TODO: Add host, URL, ...?
         }),
     ],
