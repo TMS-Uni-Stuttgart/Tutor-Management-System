3b5ef345584cd6e48d4ae0dbdd2dc57bc016ad6a
Dudrie <Dudrie@gmx.de>
2021-08-02 20:30:07 +0200

    Adjust tests for SessionService to use new TestSuite.

--- server/src/module/session/session.service.spec.ts
+++ server/src/module/session/session.service.spec.ts
@@ -1,18 +1,6 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import { TestSuite } from '../../../test/helpers/TestSuite';
 import { SessionService } from './session.service';
 
 describe('SessionService', () => {
-    let service: SessionService;
-
-    beforeEach(async () => {
-        const module: TestingModule = await Test.createTestingModule({
-            providers: [SessionService],
-        }).compile();
-
-        service = module.get<SessionService>(SessionService);
-    });
-
-    it('should be defined', () => {
-        expect(service).toBeDefined();
-    });
+    new TestSuite(SessionService);
 });
