974b4eed2a52ffca3204e71513d894f8bdfdd62b
Dudrie <Dudrie@gmx.de>
2021-08-06 15:06:12 +0200

    Fix scheincriteria inside ScheincriteriaEntity not being properly instantiated.

--- server/src/database/entities/scheincriteria.entity.ts
+++ server/src/database/entities/scheincriteria.entity.ts
@@ -2,6 +2,8 @@ import { Entity, PrimaryKey, Property } from '@mikro-orm/core';
 import { IScheinCriteria } from 'shared/model/ScheinCriteria';
 import { v4 } from 'uuid';
 import { Scheincriteria } from '../../module/scheincriteria/container/Scheincriteria';
+import { plainToClass } from 'class-transformer';
+import { ScheincriteriaContainer } from '../../module/scheincriteria/container/scheincriteria.container';
 
 @Entity()
 export class ScheincriteriaEntity {
@@ -11,12 +13,23 @@ export class ScheincriteriaEntity {
     @Property()
     name: string;
 
-    @Property({ type: 'json' })
-    criteria: Scheincriteria;
+    @Property({ type: 'json', name: 'criteria' })
+    private _criteria: Scheincriteria;
+
+    get criteria(): Scheincriteria {
+        const classType = ScheincriteriaContainer.getContainer().getBluePrint(
+            this._criteria.identifier
+        ).blueprint;
+        return plainToClass(classType, this._criteria);
+    }
+
+    set criteria(criteria: Scheincriteria) {
+        this._criteria = criteria;
+    }
 
     constructor(params: ScheincriteriaParams) {
         this.name = params.name;
-        this.criteria = params.criteria;
+        this._criteria = params.criteria;
     }
 
     toDTO(): IScheinCriteria {
