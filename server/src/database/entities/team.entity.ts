1d8a600e9b8b1436ba49d139792645c38fa8bb2e
Mykhailo <mishazelea@gmail.com>
2024-08-30 01:51:50 +0200

    Fix team deletion

--- server/src/database/entities/team.entity.ts
+++ server/src/database/entities/team.entity.ts
@@ -26,7 +26,7 @@ export class Team {
     @OneToMany(() => Student, (student) => student.team)
     students = new Collection<Student>(this);
 
-    @ManyToOne()
+    @ManyToOne(() => Tutorial, { deleteRule: 'cascade' })
     tutorial: Tutorial;
 
     get studentCount(): number {
